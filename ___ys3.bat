4f2287b00ffae1c115fa13af94e4afffc1790edd
Roberto Fontanarosa <roberto.fontanarosa@pronetics.it>
2016-07-06 10:36:23 +0200

    new parameter management for ys3 dumping/insertion/repointer script

--- ___ys3.bat
+++ ___ys3.bat
@@ -1,2 +1,12 @@
-python _ys3.py --crc32check -s "ys3/roms/Ys III - Wanderers from Ys (U) [!].smc" -d "ys3/roms/Ys III - Wanderers from Ys (I) [!].smc" -t1 "ys3/tables/Ys III - Wanderers from Ys (U) [!].tbl" -t2 "ys3/tables/Ys III - Wanderers from Ys (U) [!].tbl" -u clomax -db "ys3/db/ys3.db"
+set USER="clomax"
+set DB="ys3/db/ys3.db"
+set SOURCE="ys3/roms/Ys III - Wanderers from Ys (U) [!].smc"
+set DESTINATION="ys3/roms/Ys III - Wanderers from Ys (I) [!].smc"
+set TABLE1="ys3/tables/Ys III - Wanderers from Ys (U) [!].tbl"
+set TABLE2="ys3/tables/Ys III - Wanderers from Ys (U) [!].tbl"
+set TABLE3="ys3/tables/Ys III - Wanderers from Ys (U) [!].tbl"
+
+REM python _ys3.py --crc32check --dump -u %USER% -db %DB% -s %SOURCE% -d %DESTINATION% -t1 %TABLE1% -t2 %TABLE2%
+python _ys3.py --insert -u %USER% -db %DB% -s %SOURCE% -d %DESTINATION% -t1 %TABLE1% -t2 %TABLE2%
+
 pause
--- _ys3.py
+++ _ys3.py
@@ -14,6 +14,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--crc32check', action='store_true', default=False, help='Execute CRC32CHECK')
 parser.add_argument('--dump', action='store_true', default=False, help='Execute DUMP')
+parser.add_argument('--insert', action='store_true', default=False, help='Execute INSERTER')
 parser.add_argument('-s', '--source', action='store', dest='source_file', required=True, help='Original filename')
 parser.add_argument('-d', '--dest',  action='store', dest='dest_file', required=True, help='Destination filename')
 parser.add_argument('-t1', '--table1', action='store', dest='table1', required=True, help='Original table filename')
@@ -24,6 +25,7 @@ args = parser.parse_args()
 
 execute_crc32check = args.crc32check
 execute_dump = args.dump
+execute_inserter = args.insert
 filename = args.source_file
 filename2 = args.dest_file
 tablename = args.table1
@@ -39,13 +41,13 @@ CRC32 = '64A91E64'
 
 POINTER_BLOCK1_START = 0x280b4
 POINTER_BLOCK1_END = POINTER_BLOCK1_LIMIT = 0x28413
+POINTER_BLOCK2_START = 0x20000
+POINTER_BLOCK2_END = POINTER_BLOCK2_LIMIT = 0x20267
+
 TEXT_BLOCK1_START = 0x28418
 TEXT_BLOCK1_END = TEXT_BLOCK1_LIMIT = 0x2fc73
 TEXT_BLOCK1_SIZE = TEXT_BLOCK1_END - TEXT_BLOCK1_START
 TEXT_BLOCK1_MAX_SIZE = TEXT_BLOCK1_LIMIT - TEXT_BLOCK1_START
-
-POINTER_BLOCK2_START = 0x20000
-POINTER_BLOCK2_END = POINTER_BLOCK2_LIMIT = 0x20267
 TEXT_BLOCK2_START = 0x2026a
 TEXT_BLOCK2_END = TEXT_BLOCK2_LIMIT = 0x229d1
 TEXT_BLOCK2_SIZE = TEXT_BLOCK2_END - TEXT_BLOCK2_START
@@ -54,15 +56,15 @@ TEXT_BLOCK2_MAX_SIZE = TEXT_BLOCK2_LIMIT - TEXT_BLOCK2_START
 table = Table(tablename)
 table2 = Table(tablename2)
 
-# CHECKSUM (CRC32)
 if execute_crc32check:
+	""" CHECKSUM (CRC32) """
 	if crc32(filename) != CRC32:
 		sys.exit('ROM CHECKSUM: FAIL')
 	else:
 		print 'ROM CHECKSUM: OK'
 
-#DUMP (TXTs + SQLITE3)
 if execute_dump:
+	""" DUMP """
 	conn = sqlite3.connect(db)
 	conn.text_factory = str
 	cur = conn.cursor()
@@ -70,7 +72,7 @@ if execute_dump:
 	with open(filename, 'rb') as f:
 		# POINTERS 1
 		pointers = OrderedDict()
-		f.seek(POINTER_BLOCK1_START)	
+		f.seek(POINTER_BLOCK1_START)
 		while(f.tell() < POINTER_BLOCK1_END):
 			paddress = f.tell()
 			pvalue = f.read(2)
@@ -90,10 +92,11 @@ if execute_dump:
 				byte = f.read(1)
 				text += byte
 			text_encoded = table.encode(text, separated_byte_format=False)
+			# DUMP - DB
 			text_binary = sqlite3.Binary(text)
 			text_address = int2hex(pointer)
 			text_length = len(text_binary)
-			cur.execute('insert or replace into texts values (?, ?, ?, ?, ?, ?, 1)', (id, buffer(text_binary), text_encoded, text_address, pointer_addresses, text_length))				
+			cur.execute('insert or replace into texts values (?, ?, ?, ?, ?, ?, 1)', (id, buffer(text_binary), text_encoded, text_address, pointer_addresses, text_length))
 			# DUMP - TXT
 			with open('%s - %s.txt' % (dump_path + str(id).zfill(3), pointer_addresses), 'w') as out:
 				out.write(text_encoded)
@@ -122,10 +125,11 @@ if execute_dump:
 					byte = f.read(1)
 					text += byte
 				text_encoded = table.encode(text, separated_byte_format=True)
+				# DUMP - DB
 				text_binary = sqlite3.Binary(text)
 				text_address = int2hex(pointer)
 				text_length = len(text_binary)
-				cur.execute('insert or replace into texts values (?, ?, ?, ?, ?, ?, 2)', (id, buffer(text_binary), text_encoded, text_address, pointer_addresses, text_length))				
+				cur.execute('insert or replace into texts values (?, ?, ?, ?, ?, ?, 2)', (id, buffer(text_binary), text_encoded, text_address, pointer_addresses, text_length))
 				# DUMP - TXT
 				with open('%s - %s.txt' % (dump_path + str(id).zfill(3), pointer_addresses), 'w') as out:
 					out.write(text_encoded)
@@ -135,74 +139,75 @@ if execute_dump:
 	conn.commit()
 	conn.close()
 
-#INSERTER + REPOINTER (SQLITE3)
-conn = sqlite3.connect(db)
-conn.text_factory = str
-cur = conn.cursor()
-with open(filename2, 'r+b') as f:
-	# BLOCK 1
-	f.seek(TEXT_BLOCK1_START)
-	cur.execute("SELECT text, new_text, text_encoded, id, new_text2, address, pointer_address, size FROM texts AS t1 LEFT OUTER JOIN (SELECT * FROM trans WHERE trans.author='%s' AND trans.status = 2) AS t2 ON t1.id=t2.id_text WHERE t1.block = 1" % user_name)
-	for row in cur:
-		# INSERTER 1
-		id = row[3]
-		original_text = row[2]
-		new_text = row[4]
-		if (new_text):
-			text = new_text
-			decoded_text = table2.decode(text, True)
-		else:
-			text = original_text
-			decoded_text = table.decode(text, True)
-		new_text_address = f.tell()
-		f.seek(new_text_address)
-		f.write(decoded_text)
-		next_text_address = f.tell()
-		if next_text_address > (TEXT_BLOCK1_LIMIT + 1):
-			sys.exit('CRITICAL ERROR! TEXT_BLOCK_LIMIT! %s > %s (%s)' % (next_text_address, TEXT_BLOCK1_LIMIT, (TEXT_BLOCK1_LIMIT - next_text_address)))
-		# REPOINTER 1
-		pointer_addresses = row[6]
-		if pointer_addresses:
-			for pointer_address in pointer_addresses.split(';'):
-				if pointer_address:
-					pointer_address = hex2dec(pointer_address)
-					f.seek(pointer_address)				
-					f.write(int_address2string_address2(new_text_address-0x20000, switch=True, shift=2))
-					if pointer_address > (POINTER_BLOCK1_LIMIT + 1):
-						sys.exit('CRITICAL ERROR! POINTER_BLOCK_LIMIT! %s > %s (%s)' % (pointer_address, POINTER_BLOCK1_LIMIT, (POINTER_BLOCK1_LIMIT - pointer_address)))
-		f.seek(next_text_address)
-	# BLOCK 2
-	f.seek(TEXT_BLOCK2_START)
-	cur.execute("SELECT text, new_text, text_encoded, id, new_text2, address, pointer_address, size FROM texts AS t1 LEFT OUTER JOIN (SELECT * FROM trans WHERE trans.author='%s' AND trans.status = 2) AS t2 ON t1.id=t2.id_text WHERE t1.block = 2" % user_name)
-	for row in cur:
-		# INSERTER 2
-		id = row[3]
-		original_text = row[2]
-		new_text = row[4]
-		if (new_text):
-			text = new_text
-			decoded_text = table2.decode(text, True)
-		else:
-			text = original_text
-			decoded_text = table.decode(text, True)
-		new_text_address = f.tell()
-		f.seek(new_text_address)
-		f.write(decoded_text)
-		next_text_address = f.tell()
-		if next_text_address - 0x8000 > (TEXT_BLOCK2_LIMIT + 1):
-			sys.exit('CRITICAL ERROR! TEXT_BLOCK_LIMIT! %s > %s (%s)' % (next_text_address, TEXT_BLOCK2_LIMIT, (TEXT_BLOCK2_LIMIT - next_text_address)))
-		# REPOINTER 2
-		if id != 547:
-			padresses = row[6]
-			if padresses:
-				for paddress in padresses.split(';'):
-					if paddress:
-						paddress = hex2dec(paddress)
-						f.seek(paddress)
-						pvalue = struct.pack('H', new_text_address - 0x18000)
-						f.write(pvalue)
-						if paddress - 0x8000 > (POINTER_BLOCK2_LIMIT + 1):
-							sys.exit('CRITICAL ERROR! POINTER_BLOCK_LIMIT! %s > %s (%s)' % (paddress, POINTER_BLOCK2_LIMIT, (POINTER_BLOCK2_LIMIT - paddress)))
-		f.seek(next_text_address)
-cur.close()
-conn.close()
+if execute_inserter:
+	""" INSERTER + REPOINTER """
+	conn = sqlite3.connect(db)
+	conn.text_factory = str
+	cur = conn.cursor()
+	with open(filename2, 'r+b') as f:
+		# BLOCK 1
+		f.seek(TEXT_BLOCK1_START)
+		cur.execute("SELECT text, new_text, text_encoded, id, new_text2, address, pointer_address, size FROM texts AS t1 LEFT OUTER JOIN (SELECT * FROM trans WHERE trans.author='%s' AND trans.status = 2) AS t2 ON t1.id=t2.id_text WHERE t1.block = 1" % user_name)
+		for row in cur:
+			# INSERTER 1
+			id = row[3]
+			original_text = row[2]
+			new_text = row[4]
+			if (new_text):
+				text = new_text
+				decoded_text = table2.decode(text, True)
+			else:
+				text = original_text
+				decoded_text = table.decode(text, True)
+			new_text_address = f.tell()
+			f.seek(new_text_address)
+			f.write(decoded_text)
+			next_text_address = f.tell()
+			if next_text_address > (TEXT_BLOCK1_LIMIT + 1):
+				sys.exit('CRITICAL ERROR! TEXT_BLOCK_LIMIT! %s > %s (%s)' % (next_text_address, TEXT_BLOCK1_LIMIT, (TEXT_BLOCK1_LIMIT - next_text_address)))
+			# REPOINTER 1
+			pointer_addresses = row[6]
+			if pointer_addresses:
+				for pointer_address in pointer_addresses.split(';'):
+					if pointer_address:
+						pointer_address = hex2dec(pointer_address)
+						f.seek(pointer_address)
+						f.write(int_address2string_address2(new_text_address-0x20000, switch=True, shift=2))
+						if pointer_address > (POINTER_BLOCK1_LIMIT + 1):
+							sys.exit('CRITICAL ERROR! POINTER_BLOCK_LIMIT! %s > %s (%s)' % (pointer_address, POINTER_BLOCK1_LIMIT, (POINTER_BLOCK1_LIMIT - pointer_address)))
+			f.seek(next_text_address)
+		# BLOCK 2
+		f.seek(TEXT_BLOCK2_START)
+		cur.execute("SELECT text, new_text, text_encoded, id, new_text2, address, pointer_address, size FROM texts AS t1 LEFT OUTER JOIN (SELECT * FROM trans WHERE trans.author='%s' AND trans.status = 2) AS t2 ON t1.id=t2.id_text WHERE t1.block = 2" % user_name)
+		for row in cur:
+			# INSERTER 2
+			id = row[3]
+			original_text = row[2]
+			new_text = row[4]
+			if (new_text):
+				text = new_text
+				decoded_text = table2.decode(text, True)
+			else:
+				text = original_text
+				decoded_text = table.decode(text, True)
+			new_text_address = f.tell()
+			f.seek(new_text_address)
+			f.write(decoded_text)
+			next_text_address = f.tell()
+			if next_text_address - 0x8000 > (TEXT_BLOCK2_LIMIT + 1):
+				sys.exit('CRITICAL ERROR! TEXT_BLOCK_LIMIT! %s > %s (%s)' % (next_text_address, TEXT_BLOCK2_LIMIT, (TEXT_BLOCK2_LIMIT - next_text_address)))
+			# REPOINTER 2
+			if id != 547:
+				padresses = row[6]
+				if padresses:
+					for paddress in padresses.split(';'):
+						if paddress:
+							paddress = hex2dec(paddress)
+							f.seek(paddress)
+							pvalue = struct.pack('H', new_text_address - 0x18000)
+							f.write(pvalue)
+							if paddress - 0x8000 > (POINTER_BLOCK2_LIMIT + 1):
+								sys.exit('CRITICAL ERROR! POINTER_BLOCK_LIMIT! %s > %s (%s)' % (paddress, POINTER_BLOCK2_LIMIT, (POINTER_BLOCK2_LIMIT - paddress)))
+			f.seek(next_text_address)
+	cur.close()
+	conn.close()
