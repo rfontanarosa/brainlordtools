8b694730fbccef459ced1dfdfbfb0ff11d2528a2
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2019-12-02 23:17:56 +0100

    bof-gemini optimizations

--- _bof-gemini.py
+++ _bof-gemini.py
@@ -5,43 +5,27 @@ __maintainer__ = "Roberto Fontanarosa"
 __email__ = "robertofontanarosa@gmail.com"
 
 import sys, os, struct, sqlite3
-from collections import OrderedDict
 
-from _rhtools.utils import *
+from _rhtools.utils import hex2dec
 from _rhtools.Table2 import Table
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('--crc32check', action='store_true', default=False, help='Execute CRC32CHECK')
-parser.add_argument('--dump', action='store_true', default=False, help='Execute DUMP')
 parser.add_argument('--insert', action='store_true', default=False, help='Execute INSERTER')
-parser.add_argument('--mte_optimizer', action='store_true', default=False, help='Optimize MTE')
-parser.add_argument('-s', '--source', action='store', dest='source_file', required=True, help='Original filename')
 parser.add_argument('-d', '--dest',  action='store', dest='dest_file', required=True, help='Destination filename')
-parser.add_argument('-t1', '--table1', action='store', dest='table1', required=True, help='Original table filename')
 parser.add_argument('-t2', '--table2', action='store', dest='table2', required=True, help='Modified table filename')
-parser.add_argument('-t3', '--table3', action='store', dest='table3', required=False, help='Base original table without')
 parser.add_argument('-db', '--database',  action='store', dest='database_file', required=True, help='DB filename')
 parser.add_argument('-u', '--user', action='store', dest='user', required=True, help='')
 args = parser.parse_args()
 
-execute_crc32check = args.crc32check
-execute_dump = args.dump
 execute_inserter = args.insert
-execute_mteoptimizer = args.mte_optimizer
-filename = args.source_file
 filename2 = args.dest_file
-tablename = args.table1
 tablename2 = args.table2
-tablename3 = args.table3
 db = args.database_file
 user_name = args.user
-dump_path = 'bof/gemini/dump/'
 
 SNES_HEADER_SIZE = 0x200
 SNES_BANK_SIZE = 0x8000
-
-CRC32 = ''
 
 DICT1_POINTER_BLOCK_START = 0x180000
 DICT1_POINTER_BLOCK_END = DICT1_POINTER_BLOCK_LIMIT = 0x1801ff
@@ -60,9 +44,6 @@ TEXT_BLOCK1_END = TEXT_BLOCK1_LIMIT = 0x19ffff
 TEXT_BLOCK2_START = 0x1a0000
 TEXT_BLOCK2_END = TEXT_BLOCK2_LIMIT = 0x1affff
 
-table = Table(tablename)
-table2 = Table(tablename2)
-
 def bofBlockLimitsResolver(block):
 	""" """
 	blockLimits = (0, 0)
@@ -72,15 +53,9 @@ def bofBlockLimitsResolver(block):
 		blockLimits = (TEXT_BLOCK2_START, TEXT_BLOCK2_LIMIT, POINTER_BLOCK2_START, POINTER_BLOCK2_END, POINTER_BLOCK2_LIMIT)
 	return blockLimits
 
-if execute_crc32check:
-	""" CHECKSUM (CRC32) """
-	if crc32(filename) != CRC32:
-		sys.exit('ROM CHECKSUM: FAIL')
-	else:
-		print('ROM CHECKSUM: OK')
-
 if execute_inserter:
 	""" INSERTER """
+	table2 = Table(tablename2)
 	conn = sqlite3.connect(db)
 	conn.text_factory = str
 	cur = conn.cursor()
