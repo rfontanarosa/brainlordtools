e3af11ff3e769271f05e1dc03e9fd411e29d2b45
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2020-10-19 13:56:55 +0200

    argument parser has been improved, added specific game options, minor changes

--- mteOpt.py
+++ mteOpt.py
@@ -5,47 +5,49 @@ from shutil import copyfile
 from collections import defaultdict, Counter, OrderedDict
 
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('-m', '--min', action='store', dest='min', type=int, required=True, help='')
-parser.add_argument('-M', '--max',  action='store', dest='max', type=int, required=True, help='')
-parser.add_argument('-l', '--limit',  action='store', dest='limit', type=int, required=True, help='')
-parser.add_argument('-s', '--source', action='store', dest='source_file', required=True, help='Original filename')
-parser.add_argument('-c', '--clean', action='store', dest='clean_file', required=True, help='Clean filename')
-parser.add_argument('-d', '--dest',  action='store', dest='dest_file', required=True, help='Destination filename')
-parser.add_argument('-o', '--offset',  action='store', dest='offset', type=int, required=True, help='Starting offset')
+parent_parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(add_help=False) 
+subparsers = parser.add_subparsers(dest='cmd')
+parent_parser.add_argument('-m', '--min', action='store', dest='min', type=int, default=3, help='')
+parent_parser.add_argument('-M', '--max',  action='store', dest='max', type=int, default=8, help='')
+parent_parser.add_argument('-l', '--limit',  action='store', dest='limit', type=int, default=5, help='')
+parent_parser.add_argument('-s', '--source', action='store', dest='source_file', required=True, help='Original filename')
+parent_parser.add_argument('-c', '--clean', action='store', dest='clean_file', required=True, help='Clean filename')
+parent_parser.add_argument('-g', '--game', choices=['smrpg', 'bof', 'ys4'], help='Game specific cleaning rules')
+parser0 = subparsers.add_parser('print' , parents=[parent_parser], add_help=False)
+parser1 = subparsers.add_parser('table', parents=[parent_parser], add_help=False)
+parser1.add_argument('-d', '--dest', action='store', dest='dest_file', required=True, help='Destination filename')
+parser1.add_argument('-o', '--offset', action='store', dest='offset', type=int, default=0, help='Starting offset')
 args = parser.parse_args()
+dict_args = vars(args)
 
-MIN = args.min
-MAX = args.max
-LIMIT = args.limit
-filename = args.source_file
-filename1 = args.clean_file
-filename2 = args.dest_file
-offset = args.offset
+cmd = dict_args.get('cmd')
+MIN = dict_args.get('min')
+MAX = dict_args.get('max')
+LIMIT = dict_args.get('limit')
+filename = dict_args.get('source_file')
+filename1 = dict_args.get('clean_file')
+game = dict_args.get('game')
+filename2 = dict_args.get('dest_file')
+offset = dict_args.get('offset')
 
-SMRPG_REGEX_LIST = [
-	(re.compile(r'^.{7}'), ''),
-	(re.compile(r' {5,}'), ''),
-	(re.compile(r'[.]{3,}'), '\n'),
-	(re.compile(r'\[.+?\]'), '\n')
-]
-
-def clean_file(filename, filename1, regexList=[], allowDuplicates=True):
+def clean_file(filename, filename1, regex_list=None, allow_duplicates=True):
 	""" applica a ogni linea del file delle espressioni regolari, rimuove opzionalmente le linee duplicate """
 	with io.open(filename, mode='r', encoding="utf-8") as f, io.open(filename1, mode='w', encoding="utf-8") as f1:
-		linesSeen = set()
+		lines_seen = set()
 		for line in f.readlines():
-			for regex in regexList:
-				line = regex[0].sub(regex[1], line)
-			if allowDuplicates:
+			if regex_list:
+				for regex in regex_list:
+					line = regex[0].sub(regex[1], line)
+			if allow_duplicates:
 				f1.write(line)
-			elif line not in linesSeen:
+			elif line not in lines_seen:
 				f1.write(line)
-				linesSeen.add(line)
+				lines_seen.add(line)
 
-def extract_chunks(text, chunkSize, startPoint=0):
+def extract_chunks(text, chunk_size, start_index=0):
 	""" estrae le occorrenze di lunghezza n da una stringa """
-	return [text[i:(i+chunkSize)] for i in range(startPoint, len(text), chunkSize)]
+	return [text[i:(i+chunk_size)] for i in range(start_index, len(text), chunk_size)]
 
 def get_substrings_by_length(text, length):
 	""" estrae tutte le possibili occorrenze di lunghezza n da una stringa """
@@ -61,7 +63,7 @@ def get_occurrences_by_length(filename, length):
 	with io.open(filename, mode='r', encoding="utf-8") as f:
 		for line in f.readlines():
 			if line:
-				line = line.replace('\n', '').replace('\r', '')
+				line = line.rstrip('\r\n')
 				substrings = get_substrings_by_length(line, length)
 				for string in substrings: dictionary[string] += 1
 	return dictionary
@@ -91,7 +93,20 @@ def export_table(filename, dictionary, offset):
 			line = "%s=%s" % (b, v)
 			out.write(line + '\n')
 
-clean_file(filename, filename1, regexList=SMRPG_REGEX_LIST, allowDuplicates=False)
+regex_list = None
+if game == 'bof':
+	pass
+elif game == 'smrpg':
+	regex_list = [
+		(re.compile(r'^.{7}'), ''),
+		(re.compile(r' {5,}'), ''),
+		(re.compile(r'[.]{3,}'), '\n'),
+		(re.compile(r'\[.+?\]'), '\n')
+	]
+elif game == 'ys4':
+	pass
+
+clean_file(filename, filename1, regex_list=regex_list, allow_duplicates=False)
 dictionary = OrderedDict()
 curr_filename = filename1
 for i in range(0, LIMIT):
@@ -101,6 +116,11 @@ for i in range(0, LIMIT):
 	sorted_dicionary = sort_dict_by_value(occurrences_with_weight)
 	k, v = sorted_dicionary[0]
 	dictionary[k] = v
-	clean_file(curr_filename, next_filename, regexList=[(re.compile(re.escape(k)), '\n')], allowDuplicates=True)
+	clean_file(curr_filename, next_filename, regex_list=[(re.compile(re.escape(k)), '\n')], allow_duplicates=True)
 	curr_filename = next_filename
-export_table(filename2, dictionary, offset)
+
+if cmd == 'print':
+	print(dictionary)
+elif cmd == 'table':
+	print(dictionary)
+	export_table(filename2, dictionary, offset)
