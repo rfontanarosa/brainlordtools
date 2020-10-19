4ffd1c1a9a5168f15dc14e69a3e1deb01422e17d
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2020-10-19 16:56:47 +0200

    StringIO optimization, debug option

--- mteOpt.py
+++ mteOpt.py
@@ -3,6 +3,7 @@
 import sys, os, re, io
 from shutil import copyfile
 from collections import defaultdict, Counter, OrderedDict
+from StringIO import StringIO
 
 import argparse
 parent_parser = argparse.ArgumentParser()
@@ -13,7 +14,8 @@ parent_parser.add_argument('-M', '--max',  action='store', dest='max', type=int,
 parent_parser.add_argument('-l', '--limit',  action='store', dest='limit', type=int, default=5, help='')
 parent_parser.add_argument('-s', '--source', action='store', dest='source_file', required=True, help='Original filename')
 parent_parser.add_argument('-c', '--clean', action='store', dest='clean_file', required=True, help='Clean filename')
-parent_parser.add_argument('-g', '--game', choices=['smrpg', 'bof', 'ys4'], help='Game specific cleaning rules')
+parent_parser.add_argument('--game', choices=['smrpg', 'bof', 'ys4'], help='Game specific cleaning rules')
+parent_parser.add_argument('--debug', action='store_true', help='Enable DEBUG')
 parser0 = subparsers.add_parser('print' , parents=[parent_parser], add_help=False)
 parser1 = subparsers.add_parser('table', parents=[parent_parser], add_help=False)
 parser1.add_argument('-d', '--dest', action='store', dest='dest_file', required=True, help='Destination filename')
@@ -28,22 +30,43 @@ LIMIT = dict_args.get('limit')
 filename = dict_args.get('source_file')
 filename1 = dict_args.get('clean_file')
 game = dict_args.get('game')
+debug = dict_args.get('debug')
 filename2 = dict_args.get('dest_file')
 offset = dict_args.get('offset')
 
-def clean_file(filename, filename1, regex_list=None, allow_duplicates=True):
+def clean_file(f, f1, regex_list=None, allow_duplicates=True):
 	""" applica a ogni linea del file delle espressioni regolari, rimuove opzionalmente le linee duplicate """
-	with io.open(filename, mode='r', encoding="utf-8") as f, io.open(filename1, mode='w', encoding="utf-8") as f1:
-		lines_seen = set()
-		for line in f.readlines():
-			if regex_list:
-				for regex in regex_list:
-					line = regex[0].sub(regex[1], line)
-			if allow_duplicates:
-				f1.write(line)
-			elif line not in lines_seen:
-				f1.write(line)
-				lines_seen.add(line)
+	lines_seen = set()
+	for line in f.readlines():
+		if regex_list:
+			for regex in regex_list:
+				line = regex[0].sub(regex[1], line)
+		if allow_duplicates:
+			f1.write(line)
+		elif line not in lines_seen:
+			f1.write(line)
+			lines_seen.add(line)
+
+def clean_string(string, regex_list=None, allow_duplicates=True):
+	""" applica a ogni linea del file delle espressioni regolari, rimuove opzionalmente le linee duplicate """
+	s = StringIO(string)
+	s1 = StringIO()
+	lines_seen = set()
+	for line in s.readlines():
+		if regex_list:
+			for regex in regex_list:
+				line = regex[0].sub(regex[1], line)
+		if allow_duplicates:
+			s1.write(line)
+		elif line not in lines_seen:
+			s1.write(line)
+			lines_seen.add(line)
+	return s1.getvalue()
+
+def string_to_file(filename, s):
+	print filename
+	with io.open(filename, mode='w', encoding="utf-8") as f:
+		f.write(s)
 
 def extract_chunks(text, chunk_size, start_index=0):
 	""" estrae le occorrenze di lunghezza n da una stringa """
@@ -57,22 +80,22 @@ def get_substrings_by_length(text, length):
 			chunks += extract_chunks(text, length, i)
 	return list(filter(lambda x: len(x) == length, chunks))
 
-def get_occurrences_by_length(filename, length):
+def get_occurrences_by_length(f, length):
 	""" genera un dizionario con le occorrenze di lunghezza n """
 	dictionary = defaultdict(int)
-	with io.open(filename, mode='r', encoding="utf-8") as f:
-		for line in f.readlines():
-			if line:
-				line = line.rstrip('\r\n')
-				substrings = get_substrings_by_length(line, length)
-				for string in substrings: dictionary[string] += 1
+	for line in f.readlines():
+		if line:
+			line = line.replace('\n', '').replace('\r', '')
+			substrings = get_substrings_by_length(line, length)
+			for string in substrings: dictionary[string] += 1
 	return dictionary
 
-def get_occurrences(filename, min_length, max_length):
+def get_occurrences(f, min_length, max_length):
 	""" genera un dizionario con le occorrenze di un range di lunghezza """
 	dictionary = Counter()
 	for length in range(min_length, max_length + 1):
-		occurrences = Counter(get_occurrences_by_length(filename, length))
+		buff = StringIO(f.getvalue())
+		occurrences = Counter(get_occurrences_by_length(buff, length))
 		dictionary.update(occurrences)
 	return dictionary
 
@@ -108,18 +131,21 @@ elif game == 'smrpg':
 elif game == 'ys4':
 	pass
 
-clean_file(filename, filename1, regex_list=regex_list, allow_duplicates=False)
+with io.open(filename, mode='r', encoding="utf-8") as f, io.open(filename1, mode='w', encoding="utf-8") as f1:
+	clean_file(f, f1, regex_list=regex_list, allow_duplicates=False)
+
 dictionary = OrderedDict()
-curr_filename = filename1
-for i in range(0, LIMIT):
-	next_filename = filename1 + '.' + str(i)
-	occurrences = get_occurrences(curr_filename, MIN, MAX)
-	occurrences_with_weight = calculate_weight(occurrences)
-	sorted_dicionary = sort_dict_by_value(occurrences_with_weight)
-	k, v = sorted_dicionary[0]
-	dictionary[k] = v
-	clean_file(curr_filename, next_filename, regex_list=[(re.compile(re.escape(k)), '\n')], allow_duplicates=True)
-	curr_filename = next_filename
+with io.open(filename1, mode='r', encoding="utf-8") as f:
+	buff = StringIO(f.read())
+	for i in range(0, LIMIT):
+		occurrences = get_occurrences(buff, MIN, MAX)
+		occurrences_with_weight = calculate_weight(occurrences)
+		sorted_dicionary = sort_dict_by_value(occurrences_with_weight)
+		k, v = sorted_dicionary[0]
+		dictionary[k] = v
+		buff = StringIO(clean_string(buff.getvalue(), regex_list=[(re.compile(re.escape(k)), '\n')], allow_duplicates=True))
+		if debug:
+			string_to_file(filename1 + '.' + str(i), buff.getvalue())
 
 if cmd == 'print':
 	print(dictionary)
