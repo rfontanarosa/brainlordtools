c3174eb60075797a694f67fa3542bd11b631d4d2
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2020-10-19 11:29:10 +0200

    refactorized mte optimizer

--- mteOpt.py
+++ mteOpt.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import sys, os, re, io
+from shutil import copyfile
+from collections import defaultdict, Counter, OrderedDict
 
 import argparse
 parser = argparse.ArgumentParser()
@@ -24,11 +26,11 @@ offset = args.offset
 SMRPG_REGEX_LIST = [
 	(re.compile(r'^.{7}'), ''),
 	(re.compile(r' {5,}'), ''),
-	(re.compile(r'[.]{3,}'), ''),
+	(re.compile(r'[.]{3,}'), '\n'),
 	(re.compile(r'\[.+?\]'), '\n')
 ]
 
-def cleanFile(filename, filename1, regexList=[], allowDuplicates=True):
+def clean_file(filename, filename1, regexList=[], allowDuplicates=True):
 	""" applica a ogni linea del file delle espressioni regolari, rimuove opzionalmente le linee duplicate """
 	with io.open(filename, mode='r', encoding="utf-8") as f, io.open(filename1, mode='w', encoding="utf-8") as f1:
 		linesSeen = set()
@@ -41,91 +43,64 @@ def cleanFile(filename, filename1, regexList=[], allowDuplicates=True):
 				f1.write(line)
 				linesSeen.add(line)
 
-def extractWordsFromFile(filename):
-	""" estrae tutte le parole (sequenze separate da uno spazio) da un file """
-	words = []
+def extract_chunks(text, chunkSize, startPoint=0):
+	""" estrae le occorrenze di lunghezza n da una stringa """
+	return [text[i:(i+chunkSize)] for i in range(startPoint, len(text), chunkSize)]
+
+def get_substrings_by_length(text, length):
+	""" estrae tutte le possibili occorrenze di lunghezza n da una stringa """
+	chunks = []
+	if length > 0:
+		for i in range (0, length):
+			chunks += extract_chunks(text, length, i)
+	return list(filter(lambda x: len(x) == length, chunks))
+
+def get_occurrences_by_length(filename, length):
+	""" genera un dizionario con le occorrenze di lunghezza n """
+	dictionary = defaultdict(int)
 	with io.open(filename, mode='r', encoding="utf-8") as f:
-		lines = f.readlines()
-		for line in lines:
-			wordsToAdd = line.replace('\n', '').replace('\r', '').replace("'", " ").split(" ")
-			wordsToAdd = filter(None, wordsToAdd)
-			for word in wordsToAdd:
-				if word + ' ' in line:
-					wordsToAdd.append(word + ' ')
-					wordsToAdd.remove(word)
-			words += wordsToAdd
-	return words
-
-def extractLinesFromFile(filename):
-	""" estrae tutte le linee da un file """
-	lines = []
-	with io.open(filename, mode='r', encoding="utf-8") as f:
-		lines = f.readlines()
-	return lines
-
-def wordRegexFilter(words, regexList):
-	""" ripulisce le parole da caretteri e byte inutili """
-	if words and regexList:
-		for regex in regexList:
-			words = list(map(lambda x: regex[0].sub(regex[1], x), words))
-		words = list(filter(lambda x: x and x != '', words))
-	return words if words else []
-
-def syllableCounter(list, min=2, max=3):
-	"""  """
-	dictionary = {}
-	if list:
-		for elem in list:
-			if len(elem) >= min:
-				for i in range(min, max+1):
-					for k in range(0, len(elem)):
-						syllable = elem[k:k+i]
-						if len(syllable) >= i:
-							if syllable not in dictionary:
-								dictionary[syllable] = 0
-							dictionary[syllable] = dictionary[syllable] + 1
+		for line in f.readlines():
+			if line:
+				line = line.replace('\n', '').replace('\r', '')
+				substrings = get_substrings_by_length(line, length)
+				for string in substrings: dictionary[string] += 1
 	return dictionary
 
-def wordCounter(words, min=2, max=3):
-	"""  """
-	dictionary = {}
-	if words:
-		for word in words:
-			if len(word) in range(min, max+1):
-				if word not in dictionary:
-					dictionary[word] = 0
-				dictionary[word] = dictionary[word] + 1
+def get_occurrences(filename, min_length, max_length):
+	""" genera un dizionario con le occorrenze di un range di lunghezza """
+	dictionary = Counter()
+	for length in range(min_length, max_length + 1):
+		occurrences = Counter(get_occurrences_by_length(filename, length))
+		dictionary.update(occurrences)
 	return dictionary
 
-def calculateWeight(dictionary):
+def calculate_weight(dictionary):
 	""" crea un dizionario pesato sulla lunghezza delle parole """
 	return {k: v * (len(k) - 2) for k, v in dictionary.items()}
 
-def sortDictByValue(dictionary, reverse=True):
-	""" reversa il dizionario ordinandolo per il peso """
+def sort_dict_by_value(dictionary, reverse=True):
+	""" reversa il dizionario e lo ordina per valore """
 	return sorted(dictionary.iteritems(), key=lambda(k,v):(v,k), reverse=reverse)
 
-cleanFile(filename, filename1, regexList=SMRPG_REGEX_LIST, allowDuplicates=False)
-words = extractWordsFromFile(filename1)
-##words = extractLinesFromFile(filename1)
-#print("---------")
-filteredWords = wordRegexFilter(words, None)
-#print("---------")
-#print(filteredWords)
-dictionary = wordCounter(filteredWords, min=MIN, max=MAX)
-##dictionary = syllableCounter(filteredWords, min=MIN, max=MAX)
-#print("---------")
-#print(dictionary)
-weightDictionary = calculateWeight(dictionary)
-#print("---------")
-#print(weightDictionary)
-weightDictionaryByValue = sortDictByValue(weightDictionary)
-#print weightDictionaryByValue
-#print weightDictionaryByValue[:LIMIT]
-with io.open(filename2, mode='w', encoding="utf-8") as out:
-	for i, e in enumerate(weightDictionaryByValue[:LIMIT]):
-		n = hex(i + offset).rstrip('L')
-		b = (n + '').replace('0x', '')
-		b = b.zfill(4)
-		line = "%s=%s" % (b, e[0])
-		out.write(line + '\n')
+def export_table(filename, dictionary, offset):
+	with io.open(filename, mode='w', encoding="utf-8") as out:
+		for i, v in enumerate(dictionary):
+			n = hex(i + offset).rstrip('L')
+			b = (n + '').replace('0x', '')
+			b = b.zfill(4)
+			line = "%s=%s" % (b, v)
+			out.write(line + '\n')
+
+clean_file(filename, filename1, regexList=SMRPG_REGEX_LIST, allowDuplicates=False)
+dictionary = OrderedDict()
+curr_filename = filename1
+for i in range(0, LIMIT):
+	next_filename = filename1 + '.' + str(i)
+	occurrences = get_occurrences(curr_filename, MIN, MAX)
+	occurrences_with_weight = calculate_weight(occurrences)
+	sorted_dicionary = sort_dict_by_value(occurrences_with_weight)
+	k, v = sorted_dicionary[0]
+	dictionary[k] = v
+	clean_file(curr_filename, next_filename, regexList=[(re.compile(re.escape(k)), '\n')], allowDuplicates=True)
+	curr_filename = next_filename
+export_table(filename2, dictionary, offset)
