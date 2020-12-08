26fa8ba2a483a6f6898653106f8d7d23f5c3c3a6
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2020-12-08 00:00:41 -0600

    moved scripts into scripts folder, ffmq-ombra-utils ported to python3

--- brainlordtools/scripts/ffmq-ombra-utils.py
+++ brainlordtools/scripts/ffmq-ombra-utils.py
@@ -4,30 +4,28 @@ __version__ = ""
 __maintainer__ = "Roberto Fontanarosa"
 __email__ = "robertofontanarosa@gmail.com"
 
-import sys, os, sqlite3, time
+import sys, os, sqlite3
 from collections import OrderedDict
 
 user_name = 'clomax'
+resources_path = '../../../brainlordresources/ffmq'
 
-resources_path = '../../brainlordresources/ffmq'
+db = os.path.join(resources_path, 'db/ffmq.db')
 dump_path = os.path.join(resources_path, 'dump')
 translation_path = os.path.join(resources_path, 'translation')
 user_path = os.path.join(resources_path, user_name)
-db = os.path.join(resources_path, 'db/ffmq.db')
-
-dialogues_path = os.path.join(dump_path, 'dump_eng.txt')
-dialogues_ita_path = os.path.join(translation_path, 'dump_ita.txt')
-dialogues_user_path = os.path.join(user_path, 'dump_ita.txt')
+dump_fullpath = os.path.join(dump_path, 'dump_eng.txt')
+dump_ita_fullpath = os.path.join(translation_path, 'dump_ita.txt')
+dump_user_fullpath = os.path.join(translation_path, 'dump_ita_{}.txt'.format(user_name))
 
 import_dump= True
 export_user_translation = True
-import_user_translation = False
 
 if import_dump:
   conn = sqlite3.connect(db)
   conn.text_factory = str
   cur = conn.cursor()
-  with open(dialogues_path, 'rb') as f:
+  with open(dump_fullpath, 'r') as f:
     block = 1
     id = 0
     buffer = OrderedDict()
@@ -41,19 +39,18 @@ if import_dump:
       [text, id2] = value
       text_length = len(text)
       text_encoded = text.strip('\n\r')
-      #text_encoded = text_encoded.decode("iso-8859-1").encode("utf-8")
       cur.execute('insert or replace into texts values (?, ?, ?, ?, ?, ?, ?, ?)', (id, '', text_encoded, '', '', text_length, block, id2))
   cur.close()
   conn.commit()
   conn.close()
 
 if export_user_translation:
-  if os.path.isfile(dialogues_user_path):
-    os.remove(dialogues_user_path)
+  if os.path.isfile(dump_user_fullpath):
+    os.remove(dump_user_fullpath)
   conn = sqlite3.connect(db)
   conn.text_factory = str
   cur = conn.cursor()
-  with open(dialogues_user_path, 'ab') as f:
+  with open(dump_user_fullpath, 'a') as f:
     cur.execute("SELECT text, new_text, text_encoded, id, id2 FROM texts AS t1 LEFT OUTER JOIN (SELECT * FROM trans WHERE trans.author='%s' AND trans.status = 2) AS t2 ON t1.id=t2.id_text WHERE t1.block = 1 ORDER BY t1.id" % user_name)
     for row in cur:
       id2 = row[4]
@@ -67,29 +64,3 @@ if export_user_translation:
   cur.close()
   conn.commit()
   conn.close()
-
-"""
-if import_user_translation:
-  conn = sqlite3.connect(db)
-  conn.text_factory = str
-  cur = conn.cursor()
-  id = 1
-  date = time.time()
-  with open(dialoguesUserPath, 'rb') as f:
-    for line in f:
-      splittedLine = line.split('\t')
-      id2 = splittedLine[0].replace('{', '').replace('}', '').decode('utf-8-sig')
-      if user_name == 'lorenzooone':
-        line1 = next(f)
-        splittedLine = line1.split('\t')
-        id2 = splittedLine[0].replace('{', '').replace('}', '').replace('I:', '')
-      if len(splittedLine) == 2:
-        text = splittedLine[1].replace('\r', '').replace('\n', '')
-        id = int(id2) + 1
-        text1 = text
-        cur.execute('insert or replace into trans values (?, ?, ?, ?, ?, ?, ?)', (id, user_name, text, text1, 2, date, ''))
-      id += 1
-  cur.close()
-  conn.commit()
-  conn.close()
-"""
--- brainlordtools/scripts/smrpg-lazy-utils.py
+++ brainlordtools/scripts/smrpg-lazy-utils.py
@@ -6,12 +6,12 @@ __email__ = "robertofontanarosa@gmail.com"
 
 import sys, os, sqlite3, time
 
-userName = 'ombra'
-
+user_name = 'ombra'
 resources_path = '../../brainlordresources/smrpg'
+
 dump_path = os.path.join(resources_path, 'dump')
 translation_path = os.path.join(resources_path, 'translation')
-user_path = os.path.join(resources_path, userName)
+user_path = os.path.join(resources_path, user_name)
 db = os.path.join(resources_path, 'db/smrpg.db')
 
 dialoguesPath = os.path.join(dump_path, 'dialogues.txt')
@@ -23,7 +23,7 @@ battleDialoguesItaPath = os.path.join(translation_path, 'battleDialogues_ITA.txt
 battleDialoguesUserPath = os.path.join(user_path, 'battleDialogues.txt')
 
 importAll = False
-exportTranslationByUser = True
+exportTranslationByUser = False
 importTranslationByUser = True
 
 if importAll:
@@ -31,14 +31,14 @@ if importAll:
   conn.text_factory = str
   cur = conn.cursor()
   id = 1
-  with open(dialoguesPath, 'rb') as f:
+  with open(dialoguesPath, 'r') as f:
     for line in f:
       splittedLine = line.split('\t')
       id2 = splittedLine[0].replace('{', '').replace('}', '')
       text = splittedLine[1].replace('\r', '').replace('\n', '')
       cur.execute('insert or replace into texts values (?, ?, ?, ?, ?, ?, ?, ?)', (id, '', text, '', '', 0, 1, id2))
       id += 1
-  with open(battleDialoguesPath, 'rb') as f:
+  with open(battleDialoguesPath, 'r') as f:
     for line in f:
       splittedLine = line.split('\t')
       id2 = splittedLine[0].replace('{', '').replace('}', '')
@@ -57,8 +57,8 @@ if exportTranslationByUser:
   conn = sqlite3.connect(db)
   conn.text_factory = str
   cur = conn.cursor()
-  with open(dialoguesItaPath, 'ab') as f:
-    cur.execute("SELECT text, new_text, text_encoded, id, id2 FROM texts AS t1 LEFT OUTER JOIN (SELECT * FROM trans WHERE trans.author='%s' AND trans.status = 2) AS t2 ON t1.id=t2.id_text WHERE t1.block = 1 ORDER BY t1.id" % userName)
+  with open(dialoguesItaPath, 'a') as f:
+    cur.execute("SELECT text, new_text, text_encoded, id, id2 FROM texts AS t1 LEFT OUTER JOIN (SELECT * FROM trans WHERE trans.author='%s' AND trans.status = 2) AS t2 ON t1.id=t2.id_text WHERE t1.block = 1 ORDER BY t1.id" % user_name)
     for row in cur:
       id2 = row[4]
       original_text = row[2]
@@ -67,7 +67,7 @@ if exportTranslationByUser:
       f.write('{' + id2 + '}' + '\t' + text)
       f.write('\n')
   with open(battleDialoguesItaPath, 'ab') as f:
-    cur.execute("SELECT text, new_text, text_encoded, id, id2 FROM texts AS t1 LEFT OUTER JOIN (SELECT * FROM trans WHERE trans.author='%s' AND trans.status = 2) AS t2 ON t1.id=t2.id_text WHERE t1.block = 2 ORDER BY t1.id" % userName)
+    cur.execute("SELECT text, new_text, text_encoded, id, id2 FROM texts AS t1 LEFT OUTER JOIN (SELECT * FROM trans WHERE trans.author='%s' AND trans.status = 2) AS t2 ON t1.id=t2.id_text WHERE t1.block = 2 ORDER BY t1.id" % user_name)
     for row in cur:
       id2 = row[4]
       original_text = row[2]
@@ -85,11 +85,11 @@ if importTranslationByUser:
   cur = conn.cursor()
   id = 1
   date = time.time()
-  with open(dialoguesUserPath, 'rb') as f:
+  with open(dialoguesUserPath, 'r') as f:
     for line in f:
       splittedLine = line.split('\t')
       id2 = splittedLine[0].replace('{', '').replace('}', '').decode('utf-8-sig')
-      if userName == 'lorenzooone':
+      if user_name == 'lorenzooone':
         line1 = next(f)
         splittedLine = line1.split('\t')
         id2 = splittedLine[0].replace('{', '').replace('}', '').replace('I:', '')
@@ -97,13 +97,13 @@ if importTranslationByUser:
         text = splittedLine[1].replace('\r', '').replace('\n', '')
         id = int(id2) + 1
         text1 = text
-        cur.execute('insert or replace into trans values (?, ?, ?, ?, ?, ?, ?)', (id, userName, text, text1, 2, date, ''))
+        cur.execute('insert or replace into trans values (?, ?, ?, ?, ?, ?, ?)', (id, user_name, text, text1, 2, date, ''))
       id += 1
-  with open(battleDialoguesUserPath, 'rb') as f:
+  with open(battleDialoguesUserPath, 'r') as f:
     for line in f:
       splittedLine = line.split('\t')
       id2 = splittedLine[0].replace('{', '').replace('}', '').decode('utf-8-sig')
-      if userName == 'lorenzooone':
+      if user_name == 'lorenzooone':
         line1 = next(f)
         splittedLine = line1.split('\t')
         id2 = splittedLine[0].replace('{', '').replace('}', '').replace('I:', '')
@@ -111,7 +111,7 @@ if importTranslationByUser:
         text = splittedLine[1].replace('\r', '').replace('\n', '')
         id = int(id2) + 4097
         text1 = text
-        cur.execute('insert or replace into trans values (?, ?, ?, ?, ?, ?, ?)', (id, userName, text, text1, 2, date, ''))
+        cur.execute('insert or replace into trans values (?, ?, ?, ?, ?, ?, ?)', (id, user_name, text, text1, 2, date, ''))
       id += 1
   cur.close()
   conn.commit()
