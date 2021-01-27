c012bb187185a6059507fdff6ed51e43e66ab92d
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2021-01-26 22:40:42 -0600

    ffmq - limit checks

--- brainlordtools/ffmq.py
+++ brainlordtools/ffmq.py
@@ -9,11 +9,10 @@ CRC32 = '2C52C792'
 
 def get_csv_translated_texts(filename):
     translated_texts = OrderedDict()
-    with open(filename, 'rb') as csv_file:
+    with open(filename, 'r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             trans = row.get('trans') or row.get('text')
-            trans = trans.decode('utf-8')
             text_address = int(row['text_address'], 16)
             translated_texts[text_address] = trans
     return translated_texts
@@ -93,24 +92,32 @@ def ffmq_misc_inserter(args):
         translated_texts = get_csv_translated_texts(translation_file)
         for i, (t_address, t_value) in enumerate(translated_texts.items()):
             text = table.encode(t_value, mte_resolver=False, dict_resolver=False)
+            if len(text) != 16:
+                sys.exit("{} exceeds".format(t_value))
             write_text(f, t_address, text, length=16)
         # Items
         translation_file = os.path.join(translation_path, 'items.csv')
         translated_texts = get_csv_translated_texts(translation_file)
         for i, (t_address, t_value) in enumerate(translated_texts.items()):
             text = table.encode(t_value, mte_resolver=False, dict_resolver=False)
+            if len(text) != 12:
+                sys.exit("{} exceeds".format(t_value))
             write_text(f, t_address, text, length=12)
         # Enemy Attacks
         translation_file = os.path.join(translation_path, 'enemy_attacks.csv')
         translated_texts = get_csv_translated_texts(translation_file)
         for i, (t_address, t_value) in enumerate(translated_texts.items()):
             text = table.encode(t_value, mte_resolver=False, dict_resolver=False)
+            if len(text) != 12:
+                sys.exit("{} exceeds {} - {}".format(t_value, len(t_value), len(text)))
             write_text(f, t_address, text, length=12)
         # Enemy Names
         translation_file = os.path.join(translation_path, 'enemy_names.csv')
         translated_texts = get_csv_translated_texts(translation_file)
         for i, (t_address, t_value) in enumerate(translated_texts.items()):
             text = table.encode(t_value, mte_resolver=False, dict_resolver=False)
+            if len(text) != 16:
+                sys.exit("{} exceeds".format(t_value))
             write_text(f, t_address, text, length=16)
 
 import argparse
