e559f8ed5587312c477d142177f43581d5a0bdf5
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2021-03-15 23:33:46 -0500

    bof (gba) - removed old and useless script

--- bin/bof-gba.bat
+++
@@ -1,9 +0,0 @@
-set RESOURCE_PATH=../resources/bof/gba
-
-set DUMP_PATH=%RESOURCE_PATH%/dump
-set SOURCE="%RESOURCE_PATH%/roms/0245 - Breath of Fire (I).gba"
-set TABLE1="%RESOURCE_PATH%/tables/0245 - Breath of Fire (U).tbl"
-
-python ../brainlordtools/bof-gba.py dump -s %SOURCE% -t1 %TABLE1% -dp %DUMP_PATH%
-
-pause
