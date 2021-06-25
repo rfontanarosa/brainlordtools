9959a4fdd6b14587c1a1834961823b1173e134f4
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2021-06-25 01:19:31 -0500

    spike - updated script, python3 compatible

--- bin/spike.sh
+++ bin/spike.sh
@@ -3,12 +3,17 @@
 RESOURCE_PATH="../../brainlordresources/spike"
 
 USER="clomax"
-DUMP_PATH="$RESOURCE_PATH/dump"
-DB="$RESOURCE_PATH/db/spike.db"
+DB="$RESOURCE_PATH/db/spike.sqlite3"
 SOURCE="$RESOURCE_PATH/roms/Twisted Tales of Spike McFang, The (U) [!].sfc"
 DESTINATION="$RESOURCE_PATH/roms/Twisted Tales of Spike McFang, The (I) [!].sfc"
+
 TABLE1="$RESOURCE_PATH/tables/Twisted Tales of Spike McFang, The (U) [!].tbl"
 TABLE2="$RESOURCE_PATH/tables/Twisted Tales of Spike McFang, The (U) [!].tbl"
 
-python ../brainlordtools/spike.py dump -s "$SOURCE" -t1 "$TABLE1" -dp "$DUMP_PATH" -db "$DB"
-#python ../brainlordtools/spike.py insert -d "$DESTINATION" -t2 "$TABLE2" -db "$DB" -u "$USER"
+DUMP_TEXT_PATH="$RESOURCE_PATH/dump_text"
+
+python ../brainlordtools/spike.py expand -s "$SOURCE" -d "$DESTINATION"
+
+python ../brainlordtools/spike.py dump_text -s "$SOURCE" -t1 "$TABLE1" -dp "$DUMP_TEXT_PATH" -db "$DB"
+
+# python ../brainlordtools/spike.py insert -d "$DESTINATION" -t2 "$TABLE2" -db "$DB" -u "$USER"
--- brainlordtools/spike.py
+++ brainlordtools/spike.py
@@ -4,164 +4,122 @@ __version__ = ""
 __maintainer__ = "Roberto Fontanarosa"
 __email__ = "robertofontanarosa@gmail.com"
 
-import sys, os, struct, sqlite3
+import sys, os, shutil, struct, sqlite3
 from collections import OrderedDict
 
-from rhtools.utils import crc32, int2byte, int2hex, byte2int, hex2dec
-from rhtools.Table import Table
-
-from rhtools.HexByteConversion import ByteToHex
+from rhtools.utils import crc32, int2byte, int2hex
+from rhtools3.db import insert_text, convert_to_binary
+from rhtools.dump import read_text
+from rhtools3.Table import Table
 
 SNES_HEADER_SIZE = 0x200
 SNES_BANK_SIZE = 0x8000
 
 CRC32 = '8C2068D1'
 
-POINTER_BLOCKS = (
-	(0x930, 0x97d),
-	(0x10104, 0x10109),
-	(0x1010c, 0x10133),
-	(0x10468, 0x10497),
-	(0x1068e, 0x106ad),
-	(0x1228f, 0x122ae),
-	(0x12628, 0x12657)
-)
+# POINTER_BLOCKS = (
+#     (0x930, 0x97d),
+#     (0x10104, 0x10109),
+#     (0x1010c, 0x10133),
+#     (0x10468, 0x10497),
+#     (0x1068e, 0x106ad),
+#     (0x1228f, 0x122ae),
+#     (0x12628, 0x12657)
+# )
 
-POINTER_BLOCK_START = 0x62003
-#POINTER_BLOCK_END = POINTER_BLOCK_LIMIT = 0x622b4
-POINTER_BLOCK_END = POINTER_BLOCK_LIMIT = 0x622c3
-
-TEXT_BLOCK1_START = 0x6277a
-TEXT_BLOCK1_END = TEXT_BLOCK1_LIMIT = 0x19ffff
+POINTER_BLOCK_1 = (0x62003, 0x6208f)
+POINTER_BLOCK_2 = (0x62090, 0x622c3)
 
 EXP_START = 0x107f50
 
-def spike_read_text(f, end_byte=0x00):
-	text = b''
-	byte = b'1'
-	while not byte2int(byte) == end_byte:
-		if byte2int(byte) == 0xf4:
-			byte = f.read(1)
-			text += byte
-		if byte2int(byte) == 0xfa:
-			byte = f.read(3)
-			text += byte
-		byte = f.read(1)
-		text += byte
-	return text
+def spike_text_dumper(args):
+    source_file = args.source_file
+    table1_file = args.table1
+    dump_path = args.dump_path
+    db = args.database_file
+    if not args.no_crc32_check and crc32(source_file) != CRC32:
+        sys.exit('SOURCE ROM CHECKSUM FAILED!')
+    table1 = Table(table1_file)
+    conn = sqlite3.connect(db)
+    conn.text_factory = str
+    cur = conn.cursor()
+    shutil.rmtree(dump_path, ignore_errors=False)
+    os.mkdir(dump_path)
+    id = 1
+    with open(source_file, 'rb') as f:
+        pointers = OrderedDict()
+        # for pointer_block in POINTER_BLOCKS:
+        #     f.seek(pointer_block[0])
+        #     while f.tell() < pointer_block[1]:
+        #         p_offset = f.tell()
+        #         pointer = f.read(2)
+        #         p_value = struct.unpack('H', pointer)[0] + 0x10000 - 0x8000
+        #         if p_value > 0:
+        #             pointers.setdefault(p_value, []).append(p_offset)
+        # READ POINTERS BLOCK 2
+        f.seek(POINTER_BLOCK_2[0])
+        while f.tell() < POINTER_BLOCK_2[1]:
+            p_offset = f.tell()
+            pointer = f.read(3)
+            p_value = struct.unpack('i', pointer[:3] + b'\x00')[0] - 0x868000
+            if p_value > 0:
+                pointers.setdefault(p_value, []).append(p_offset)
+        # READ TEXT BLOCK 2
+        for i, (taddress, paddresses) in enumerate(pointers.items()):
+            pointer_addresses = ';'.join(hex(x) for x in paddresses)
+            f.seek(taddress)
+            text = read_text(f, taddress, end_byte=b'\xf0', cmd_list={b'\xf4': 2, b'\xf6': 1, b'\xf8': 1, b'\xfa': 4, b'\xfc': 1, b'\xfe': 1, b'\xff': 1})
+            text_decoded = table1.decode(text, cmd_list={0xf4: 2, 0xf6: 1, 0xf8: 1, 0xfa: 4, 0xfc: 1, 0xfe: 1, 0xff: 1})
+            # dump - db
+            insert_text(cur, id, convert_to_binary(text), text_decoded, taddress, pointer_addresses, 1, id)
+            # dump - txt
+            filename = os.path.join(dump_path, 'dump_eng.txt')
+            with open(filename, 'a+') as out:
+                out.write(str(id) + ' - ' + pointer_addresses + '\n' + text_decoded + "\n\n")
+            id += 1
+        cur.close()
+        conn.commit()
+        conn.close()
 
-def pointerValue2bytes(pointer):
-	"""  """
-	pStr = format(pointer + 0x900000, 'x')
-	pBytes = bytearray.fromhex(pStr)
-	return int2byte(pBytes[2]) + int2byte(pBytes[1]) + int2byte(pBytes[0])
-
-def spike_dumper(args):
-	""" DUMP """
+def spike_expander(args):
 	source_file = args.source_file
-	table1_file = args.table1
-	dump_path = args.dump_path
-	db = args.database_file
-	if crc32(source_file) != CRC32:
-		sys.exit('SOURCE ROM CHECKSUM FAILED!')
-	table1 = Table(table1_file)
-	conn = sqlite3.connect(db)
-	conn.text_factory = str
-	cur = conn.cursor()
-	id = 1
-	with open(source_file, 'rb') as f:
-		pointers = OrderedDict()
-		for pointer_block in POINTER_BLOCKS:
-			f.seek(pointer_block[0])
-			while f.tell() < pointer_block[1]:
-				p_offset = f.tell()
-				pointer = f.read(2)
-				p_value = struct.unpack('H', pointer)[0] + 0x10000 - 0x8000
-				if p_value > 0:
-					pointers.setdefault(p_value, []).append(p_offset)
-		# TEXT POINTERS
-		f.seek(POINTER_BLOCK_START)
-		while f.tell() < POINTER_BLOCK_END:
-			p_offset = f.tell()
-			pointer = f.read(3)
-			p_value = struct.unpack('i', pointer[:3] + '\x00')[0] - 0x868000
-			if p_value > 0:
-				pointers.setdefault(p_value, []).append(p_offset)
-		# TEXT 1
-		for taddress, paddresses in pointers.iteritems():
-			pointer_addresses = ';'.join(str(int2hex(x)) for x in paddresses)
-			f.seek(taddress)
-			text = b''
-			text = spike_read_text(f, end_byte=table1.getNewline())
-			text_encoded = table1.encode(text, cmd_list=[(0xf4, 2), (0xf6, 1), (0xf8, 1), (0xfa, 4), (0xfc, 1), (0xfe, 1), (0xff, 1)])
-			text_binary = sqlite3.Binary(text)
-			text_address = int2hex(taddress)
-			text_length = len(text_binary)
-			cur.execute('insert or replace into texts values (?, ?, ?, ?, ?, ?, ?, ?)', (id, buffer(text_binary), text_encoded, text_address, pointer_addresses, text_length, 1, id))
-			# DUMP - TXT
-			"""
-			with open('%s - %d.txt' % (dump_path + str(id).zfill(4), len(pointers[pointer])), 'w') as out:
-				out.write(text_encoded)
-				pass
-			"""
-			id += 1
-		cur.close()
-		conn.commit()
-		conn.close()
-
-def spike_inserter(args):
-	""" EXPANDER """
-	with open(filename2, 'r+b') as f:
-		f.seek(EXP_START)
-		f.write(int2byte(0) * 32768)
-	""" INSERTER """
-	conn = sqlite3.connect(db)
-	conn.text_factory = str
-	cur = conn.cursor()
-	with open(filename2, 'r+b') as f:
-		f.seek(EXP_START)
-		cur.execute("SELECT text, new_text, text_encoded, id, new_text2, address, pointer_address, size FROM texts AS t1 LEFT OUTER JOIN (SELECT * FROM trans WHERE trans.author='%s' AND trans.status = 2) AS t2 ON t1.id=t2.id_text WHERE t1.block = %d" % (user_name, 1))
-		for row in cur:
-			# INSERTER
-			id = row[3]
-			original_text = row[2]
-			new_text = row[4]
-			if (new_text):
-				text = new_text
-			else:
-				text = original_text
-			decoded_text = table2.decode(text)
-			new_text_address = f.tell()
-			f.seek(new_text_address)
-			f.write(decoded_text)
-			next_text_address = f.tell()
-			# REPOINTER
-			pointer_addresses = row[6]
-			if pointer_addresses:
-				for pointer_address in pointer_addresses.split(';'):
-					if pointer_address:
-						pointer_address = hex2dec(pointer_address)
-						f.seek(pointer_address)
-						pvalue = pointerValue2bytes(new_text_address)
-						f.write(pvalue)
-			f.seek(next_text_address)
-	cur.close()
-	conn.close()
+	dest_file = args.dest_file
+	if os.path.getsize(source_file) == os.path.getsize(dest_file):
+		shutil.copy(source_file, dest_file)
+		with open(dest_file, 'r+b') as f:
+			f.seek(0, 2)
+			f.write(b'\x00' * 32768)
 
 import argparse
 parser = argparse.ArgumentParser()
+parser.add_argument('--no_crc32_check', action='store_true', dest='no_crc32_check', required=False, default=False, help='CRC32 Check')
+parser.set_defaults(func=None)
 subparsers = parser.add_subparsers()
-a_parser = subparsers.add_parser('dump', help='Execute DUMP')
-a_parser.add_argument('-s', '--source', action='store', dest='source_file', required=True, help='Original filename')
-a_parser.add_argument('-t1', '--table1', action='store', dest='table1', help='Original table filename')
-a_parser.add_argument('-dp', '--dump_path', action='store', dest='dump_path', help='Dump path')
-a_parser.add_argument('-db', '--database', action='store', dest='database_file', help='DB filename')
-a_parser.set_defaults(func=spike_dumper)
-b_parser = subparsers.add_parser('insert', help='Execute INSERTER')
-b_parser.add_argument('-d', '--dest', action='store', dest='dest_file', required=True, help='Destination filename')
-b_parser.add_argument('-t2', '--table2', action='store', dest='table2', help='Modified table filename')
-b_parser.add_argument('-db', '--database', action='store', dest='database_file', help='DB filename')
-b_parser.add_argument('-u', '--user', action='store', dest='user', help='')
-b_parser.set_defaults(func=spike_inserter)
+dump_text_parser = subparsers.add_parser('dump_text', help='Execute TEXT DUMP')
+dump_text_parser.add_argument('-s', '--source', action='store', dest='source_file', required=True, help='Original filename')
+dump_text_parser.add_argument('-t1', '--table1', action='store', dest='table1', help='Original table filename')
+dump_text_parser.add_argument('-dp', '--dump_path', action='store', dest='dump_path', help='Dump path')
+dump_text_parser.add_argument('-db', '--database', action='store', dest='database_file', help='DB filename')
+dump_text_parser.set_defaults(func=spike_text_dumper)
+# insert_text_parser = subparsers.add_parser('insert_text', help='Execute TEXT INSERTER')
+# insert_text_parser.add_argument('-s', '--source', action='store', dest='source_file', required=True, help='Original filename')
+# insert_text_parser.add_argument('-d', '--dest', action='store', dest='dest_file', required=True, help='Destination filename')
+# insert_text_parser.add_argument('-t2', '--table2', action='store', dest='table2', help='Modified table filename')
+# insert_text_parser.add_argument('-tp', '--translation_path', action='store', dest='translation_path', help='Translation path')
+# insert_text_parser.add_argument('-db', '--database', action='store', dest='database_file', help='DB filename')
+# insert_text_parser.add_argument('-u', '--user', action='store', dest='user', help='')
+# insert_text_parser.set_defaults(func=spike_text_inserter)
+expand_parser = subparsers.add_parser('expand', help='Execute EXPANDER')
+expand_parser.add_argument('-s', '--source', action='store', dest='source_file', required=True, help='Original filename')
+expand_parser.add_argument('-d', '--dest', action='store', dest='dest_file', required=True, help='Destination filename')
+expand_parser.set_defaults(func=spike_expander)
 args = parser.parse_args()
 args.func(args)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    if args.func:
+        args.func(args)
+    else:
+        parser.print_help()
+
