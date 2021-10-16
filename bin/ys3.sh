3f30c6311b3007f4242d41d741b0566b14d00cc8
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2021-10-15 23:04:24 -0500

    ys3 - inserter has been rewritten, block fix

--- bin/ys3.sh
+++ bin/ys3.sh
@@ -6,8 +6,8 @@ TOOLS_PATH="$BRAINLORD_PATH/brainlordtools/brainlordtools"
 
 USER="clomax"
 DB="$RESOURCE_PATH/db/ys3.sqlite3"
-SOURCE="$RESOURCE_PATH/roms/Ys III - Wanderers from Ys (U) [!].smc"
-DESTINATION="$RESOURCE_PATH/roms/Ys III - Wanderers from Ys (I) [!].smc"
+SOURCE="$RESOURCE_PATH/roms/Ys III - Wanderers from Ys (U) [!].sfc"
+DESTINATION="$RESOURCE_PATH/roms/Ys III - Wanderers from Ys (I) [!].sfc"
 
 TABLE1="$RESOURCE_PATH/tables/Ys III - Wanderers from Ys (U) [!].tbl"
 TABLE2="$RESOURCE_PATH/tables/Ys III - Wanderers from Ys (U) [!].tbl"
@@ -20,9 +20,11 @@ DUMP_GFX_PATH="$RESOURCE_PATH/dump_gfx"
 TRANSLATION_TEXT_PATH="$RESOURCE_PATH/translation_text"
 TANSLATION_GFX_PATH="$RESOURCE_PATH/translation_gfx"
 
+python "$TOOLS_PATH/_utils.py" file_copy -s "$SOURCE" -d "$DESTINATION"
+
 python "$TOOLS_PATH/ys3.py" dump_text -s "$SOURCE" -t1 "$TABLE1" -dp "$DUMP_TEXT_PATH" -db "$DB"
 python "$TOOLS_PATH/ys3.py" dump_gfx -s "$SOURCE" -dp "$DUMP_GFX_PATH"
 python "$TOOLS_PATH/ys3.py" dump_misc -s "$SOURCE" -t1 "$TABLE3" -dp "$DUMP_MISC_PATH"
 
-# python "$TOOLS_PATH/ys3.py" insert_text -s "$SOURCE" -d "$DESTINATION" -t2 "$TABLE2" -tp "$TRANSLATION_TEXT_PATH" -db "$DB" -u "$USER"
+python "$TOOLS_PATH/ys3.py" insert_text -s "$SOURCE" -d "$DESTINATION" -t2 "$TABLE2" -tp "$TRANSLATION_TEXT_PATH" -db "$DB" -u "$USER"
 # python "$TOOLS_PATH/ys3.py" insert_gfx -d "$DESTINATION" -tp "$TANSLATION_GFX_PATH"
--- brainlordtools/ys3.py
+++ brainlordtools/ys3.py
@@ -7,9 +7,10 @@ __email__ = "robertofontanarosa@gmail.com"
 import csv, os, shutil, sqlite3, struct, sys
 from collections import OrderedDict
 
-from rhtools.utils import crc32, hex2dec, int_address2string_address2
-from rhtools3.db import insert_text, convert_to_binary
+from rhtools.utils import crc32
+from rhtools3.db import insert_text, convert_to_binary, select_translation_by_author
 from rhtools.dump import read_text, dump_binary
+from rhtools.snes_utils import snes2pc_lorom, pc2snes_lorom
 from rhtools3.Table import Table
 
 CRC32 = '64A91E64'
@@ -21,19 +22,13 @@ POINTER_BLOCKS = (
     (0x20000, 0x20267)
 )
 
-POINTER_BLOCK1_START = 0x280b4
-POINTER_BLOCK1_END = POINTER_BLOCK1_LIMIT = 0x28413
-POINTER_BLOCK2_START = 0x20000
-POINTER_BLOCK2_END = POINTER_BLOCK2_LIMIT = 0x20267
-
 TEXT_BLOCK1_START = 0x28418
 TEXT_BLOCK1_END = TEXT_BLOCK1_LIMIT = 0x2fc73
-TEXT_BLOCK1_SIZE = TEXT_BLOCK1_END - TEXT_BLOCK1_START
-TEXT_BLOCK1_MAX_SIZE = TEXT_BLOCK1_LIMIT - TEXT_BLOCK1_START
 TEXT_BLOCK2_START = 0x2026a
 TEXT_BLOCK2_END = TEXT_BLOCK2_LIMIT = 0x229d1
-TEXT_BLOCK2_SIZE = TEXT_BLOCK2_END - TEXT_BLOCK2_START
-TEXT_BLOCK2_MAX_SIZE = TEXT_BLOCK2_LIMIT - TEXT_BLOCK2_START
+
+#0xc4450=POINTER DICTIONARY
+#0x17c73=DICTIONARY
 
 def ys3_text_dumper(args):
     source_file = args.source_file
@@ -51,13 +46,13 @@ def ys3_text_dumper(args):
     id = 1
     with open(source_file, 'rb') as f:
         # READ POINTER BLOCKS
-        for index, pointer_block in enumerate(POINTER_BLOCKS):
+        for block, pointer_block in enumerate(POINTER_BLOCKS):
             pointers = OrderedDict()
             f.seek(pointer_block[0])
             while f.tell() < pointer_block[1]:
                 p_offset = f.tell()
                 p_value = 0
-                if index == 0:
+                if block == 0:
                     pointer = f.read(2)
                     p_value = struct.unpack('H', pointer)[0] + 0x20000
                 else:
@@ -71,11 +66,11 @@ def ys3_text_dumper(args):
                 text = read_text(f, taddress, end_byte=b'\xff', cmd_list={b'\xf0': 2, b'\xf1': 2, b'\xf2': 1, b'\xf3': 1, b'\xf6': 1, b'\xf7': 1})
                 text_decoded = table1.decode(text, cmd_list={0xf0: 2, 0xf1: 2, 0xf2: 1, 0xf3: 1, 0xf6: 1, 0xf7: 1})
                 # dump - db
-                insert_text(cur, id, convert_to_binary(text), text_decoded, taddress, pointer_addresses, 1, id)
+                insert_text(cur, id, convert_to_binary(text), text_decoded, taddress, pointer_addresses, block + 1, id)
                 # dump - txt
                 filename = os.path.join(dump_path, 'dump_eng.txt')
                 with open(filename, 'a+') as out:
-                    out.write(str(id) + ' - ' + pointer_addresses + '\n' + text_decoded + "\n\n")
+                    out.write('{} - {} - {}\n{}\n\n'.format(id, hex(taddress), pointer_addresses, text_decoded))
                 id += 1
     cur.close()
     conn.commit()
@@ -113,83 +108,73 @@ def ys3_gfx_dumper(args):
         dump_binary(f, GFX_BLOCK[0], GFX_BLOCK[1], dump_path, 'gfx_1.bin')
 
 def ys3_text_inserter(args):
-	dest_file = args.dest_file
-	table1_file = args.table1
-	table2_file = args.table2
-	db = args.database_file
-	user_name = args.user
-	table1 = Table(table1_file)
-	table2 = Table(table2_file)
-	conn = sqlite3.connect(db)
-	conn.text_factory = str
-	cur = conn.cursor()
-	with open(dest_file, 'r+b') as f:
-		# BLOCK 1
-		f.seek(TEXT_BLOCK1_START)
-		cur.execute("SELECT text, new_text, text_encoded, id, new_text2, address, pointer_address, size FROM texts AS t1 LEFT OUTER JOIN (SELECT * FROM trans WHERE trans.author='%s' AND trans.status = 2) AS t2 ON t1.id=t2.id_text WHERE t1.block = 1" % user_name)
-		for row in cur:
-			# INSERTER 1
-			id = row[3]
-			original_text = row[2]
-			new_text = row[4]
-			if (new_text):
-				text = new_text
-				decoded_text = table2.decode(text, True)
-			else:
-				text = original_text
-				decoded_text = table1.decode(text, True)
-			new_text_address = f.tell()
-			f.seek(new_text_address)
-			f.write(decoded_text)
-			next_text_address = f.tell()
-			if next_text_address > (TEXT_BLOCK1_LIMIT + 1):
-				sys.exit('CRITICAL ERROR! TEXT_BLOCK_LIMIT! %s > %s (%s)' % (next_text_address, TEXT_BLOCK1_LIMIT, (TEXT_BLOCK1_LIMIT - next_text_address)))
-			# REPOINTER 1
-			pointer_addresses = row[6]
-			if pointer_addresses:
-				for pointer_address in pointer_addresses.split(';'):
-					if pointer_address:
-						pointer_address = hex2dec(pointer_address)
-						f.seek(pointer_address)
-						f.write(int_address2string_address2(new_text_address-0x20000, switch=True, shift=2))
-						if pointer_address > (POINTER_BLOCK1_LIMIT + 1):
-							sys.exit('CRITICAL ERROR! POINTER_BLOCK_LIMIT! %s > %s (%s)' % (pointer_address, POINTER_BLOCK1_LIMIT, (POINTER_BLOCK1_LIMIT - pointer_address)))
-			f.seek(next_text_address)
-		# BLOCK 2
-		f.seek(TEXT_BLOCK2_START)
-		cur.execute("SELECT text, new_text, text_encoded, id, new_text2, address, pointer_address, size FROM texts AS t1 LEFT OUTER JOIN (SELECT * FROM trans WHERE trans.author='%s' AND trans.status = 2) AS t2 ON t1.id=t2.id_text WHERE t1.block = 2" % user_name)
-		for row in cur:
-			# INSERTER 2
-			id = row[3]
-			original_text = row[2]
-			new_text = row[4]
-			if (new_text):
-				text = new_text
-				decoded_text = table2.decode(text, True)
-			else:
-				text = original_text
-				decoded_text = table1.decode(text, True)
-			new_text_address = f.tell()
-			f.seek(new_text_address)
-			f.write(decoded_text)
-			next_text_address = f.tell()
-			if next_text_address - 0x8000 > (TEXT_BLOCK2_LIMIT + 1):
-				sys.exit('CRITICAL ERROR! TEXT_BLOCK_LIMIT! %s > %s (%s)' % (next_text_address, TEXT_BLOCK2_LIMIT, (TEXT_BLOCK2_LIMIT - next_text_address)))
-			# REPOINTER 2
-			if id != 547:
-				padresses = row[6]
-				if padresses:
-					for paddress in padresses.split(';'):
-						if paddress:
-							paddress = hex2dec(paddress)
-							f.seek(paddress)
-							pvalue = struct.pack('H', new_text_address - 0x18000)
-							f.write(pvalue)
-							if paddress - 0x8000 > (POINTER_BLOCK2_LIMIT + 1):
-								sys.exit('CRITICAL ERROR! POINTER_BLOCK_LIMIT! %s > %s (%s)' % (paddress, POINTER_BLOCK2_LIMIT, (POINTER_BLOCK2_LIMIT - paddress)))
-			f.seek(next_text_address)
-	cur.close()
-	conn.close()
+    dest_file = args.dest_file
+    table2_file = args.table2
+    db = args.database_file
+    user_name = args.user
+    table2 = Table(table2_file)
+    conn = sqlite3.connect(db)
+    conn.text_factory = str
+    cur = conn.cursor()
+    with open(dest_file, 'r+b') as f:
+        # BLOCK 1
+        f.seek(TEXT_BLOCK1_START)
+        rows = select_translation_by_author(cur, user_name, ['1'])
+        for row in rows:
+            # INSERTER 1
+            id = row[0]
+            text_decoded = row[2]
+            translation = row[5]
+            text = translation if translation else text_decoded
+            text_encoded = table2.encode(text)
+            new_text_address = f.tell()
+            f.seek(new_text_address)
+            f.write(text_encoded)
+            f.write(b'\xff')
+            next_text_address = f.tell()
+            if next_text_address > TEXT_BLOCK1_LIMIT:
+                sys.exit('CRITICAL ERROR! TEXT_BLOCK_LIMIT! %s > %s (%s)' % (next_text_address, TEXT_BLOCK1_LIMIT, (TEXT_BLOCK1_LIMIT - next_text_address)))
+            # REPOINTER 1
+            pointer_addresses = row[4]
+            if pointer_addresses:
+                pvalue = struct.pack('i', pc2snes_lorom(new_text_address))[:2]
+                for pointer_address in pointer_addresses.split(';'):
+                    if pointer_address:
+                        pointer_address = int(pointer_address, 16)
+                        f.seek(pointer_address)
+                        f.write(pvalue)
+            f.seek(next_text_address)
+        # BLOCK 2
+        f.seek(TEXT_BLOCK2_START)
+        rows = select_translation_by_author(cur, user_name, ['2'])
+        for row in rows:
+            # INSERTER 2
+            id = row[0]
+            if id in (547, 718):
+                continue
+            text_decoded = row[2]
+            translation = row[5]
+            text = translation if translation else text_decoded
+            text_encoded = table2.encode(text)
+            new_text_address = f.tell()
+            f.seek(new_text_address)
+            f.write(text_encoded)
+            f.write(b'\xff')
+            next_text_address = f.tell()
+            if next_text_address > TEXT_BLOCK2_LIMIT:
+                sys.exit('CRITICAL ERROR! 1TEXT_BLOCK_LIMIT! %s > %s (%s)' % (next_text_address, TEXT_BLOCK2_LIMIT, (TEXT_BLOCK2_LIMIT - next_text_address)))
+            # REPOINTER 2
+            pointer_addresses = row[4]
+            if pointer_addresses:
+                for pointer_address in pointer_addresses.split(';'):
+                    if pointer_address:
+                        pointer_address = int(pointer_address, 16)
+                        f.seek(pointer_address)
+                        pvalue = struct.pack('i', pc2snes_lorom(new_text_address))[:2]
+                        f.write(pvalue)
+            f.seek(next_text_address)
+    cur.close()
+    conn.close()
 
 import argparse
 parser = argparse.ArgumentParser()
@@ -203,9 +188,11 @@ dump_text_parser.add_argument('-dp', '--dump_path', action='store', dest='dump_p
 dump_text_parser.add_argument('-db', '--database', action='store', dest='database_file', help='DB filename')
 dump_text_parser.set_defaults(func=ys3_text_dumper)
 insert_text_parser = subparsers.add_parser('insert_text', help='Execute INSERTER')
+insert_text_parser.add_argument('-s', '--source', action='store', dest='source_file', required=True, help='Original filename')
 insert_text_parser.add_argument('-d', '--dest', action='store', dest='dest_file', required=True, help='Destination filename')
 insert_text_parser.add_argument('-t1', '--table1', action='store', dest='table1', help='Original table filename')
 insert_text_parser.add_argument('-t2', '--table2', action='store', dest='table2', help='Modified table filename')
+insert_text_parser.add_argument('-tp', '--translation_path', action='store', dest='translation_path', help='Translation path')
 insert_text_parser.add_argument('-db', '--database', action='store', dest='database_file', help='DB filename')
 insert_text_parser.add_argument('-u', '--user', action='store', dest='user', help='')
 insert_text_parser.set_defaults(func=ys3_text_inserter)
@@ -218,8 +205,6 @@ dump_misc_parser.add_argument('-s', '--source', action='store', dest='source_fil
 dump_misc_parser.add_argument('-t1', '--table1', action='store', dest='table1', help='Original table filename')
 dump_misc_parser.add_argument('-dp', '--dump_path', action='store', dest='dump_path', help='Dump path')
 dump_misc_parser.set_defaults(func=ys3_misc_dumper)
-args = parser.parse_args()
-args.func(args)
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -227,4 +212,3 @@ if __name__ == "__main__":
         args.func(args)
     else:
         parser.print_help()
-
