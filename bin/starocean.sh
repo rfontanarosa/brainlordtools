2b9e591f82da28d0e870f5c2cdc40f22d6ef1bf0
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-09-18 23:10:39 +0200

    star ocean - gfx importer/exporter (font, vwf)

--- bin/starocean.sh
+++ bin/starocean.sh
@@ -2,7 +2,10 @@ BRAINLORD_PATH="/Users/robertofontanarosa/git"
 RESOURCE_PATH="$BRAINLORD_PATH/brainlordresources/starocean"
 TOOLS_PATH="$BRAINLORD_PATH/brainlordtools/brainlordtools"
 
+SOURCE="$RESOURCE_PATH/roms/Star_Ocean_ES.sfc"
 DESTINATION="$RESOURCE_PATH/roms/patched_Star_Ocean_ES.sfc"
+
+DUMP_GFX_PATH="$RESOURCE_PATH/dump_gfx"
 
 TANSLATION_GFX_PATH="$RESOURCE_PATH/translation_gfx"
 
@@ -10,8 +13,10 @@ CURRENT_PATH=$PWD
 cd /Users/robertofontanarosa/git/Romhacking
 source ./venv/bin/activate
 python -m starocean.main extract $RESOURCE_PATH/roms/Star_Ocean_U.sfc $RESOURCE_PATH/dump_text/dump_en.txt
-#python -m starocean.main reinsert $RESOURCE_PATH/roms/Star_Ocean_ES.sfc $RESOURCE_PATH/dump_text/dump_en.txt
+#python -m starocean.main reinsert $RESOURCE_PATH/roms/Star_Ocean_ES.sfc $RESOURCE_PATH/translation_text/dump_it.txt
 deactivate
 cd $CURRENT_PATH
+
+python "$TOOLS_PATH/starocean.py" dump_gfx -s "$SOURCE" -dp "$DUMP_GFX_PATH"
 
 python "$TOOLS_PATH/starocean.py" insert_gfx -d "$DESTINATION" -tp "$TANSLATION_GFX_PATH"
--- brainlordtools/starocean.py
+++ brainlordtools/starocean.py
@@ -4,18 +4,35 @@ __version__ = ""
 __maintainer__ = "Roberto Fontanarosa"
 __email__ = "robertofontanarosa@gmail.com"
 
-from rhtools.dump import insert_binary
+import os, shutil
+from rhtools.dump import dump_binary, insert_binary
+
+def starocean_gfx_dumper(args):
+    source_file = args.source_file
+    dump_path = args.dump_path
+    # if crc32(source_file) != CRC32:
+    #     sys.exit('SOURCE ROM CHECKSUM FAILED!')
+    shutil.rmtree(dump_path, ignore_errors=True)
+    os.mkdir(dump_path)
+    with open(source_file, 'rb') as f:
+        dump_binary(f, 0x3f0000, 0x3f0000 + 3392, dump_path, '3F0000_font.bin')
+        dump_binary(f, 0x3f0d40, 0x3f0d40 + (4 * 13) + 1, dump_path, '3F0d40_font_vwf.bin')
 
 def starocean_gfx_inserter(args):
     dest_file = args.dest_file
     translation_path = args.translation_path
     with open(dest_file, 'r+b') as f:
         insert_binary(f, 0x3f0000, 0x3f0000 + 3392, translation_path, '3F0000_font_ita.bin')
+        insert_binary(f, 0x3f0d40, 0x3f0d40 + (4 * 13) + 1, translation_path, '3F0d40_font_vwf.bin')
 
 import argparse
 parser = argparse.ArgumentParser()
 parser.set_defaults(func=None)
 subparsers = parser.add_subparsers()
+dump_gfx_parser = subparsers.add_parser('dump_gfx', help='Execute GFX DUMP')
+dump_gfx_parser.add_argument('-s', '--source', action='store', dest='source_file', required=True, help='Original filename')
+dump_gfx_parser.add_argument('-dp', '--dump_path', action='store', dest='dump_path', help='Dump path')
+dump_gfx_parser.set_defaults(func=starocean_gfx_dumper)
 insert_gfx_parser = subparsers.add_parser('insert_gfx', help='Execute GFX INSERTER')
 insert_gfx_parser.add_argument('-d', '--dest', action='store', dest='dest_file', required=True, help='Destination filename')
 insert_gfx_parser.add_argument('-tp', '--translation_path', action='store', dest='translation_path', help='Translation path')
