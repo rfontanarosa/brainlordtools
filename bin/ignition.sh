9e720f190880009ef992601f8c2054a1a3b2442b
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-10-27 00:38:13 +0200

    used $HOME variable in some scripts

--- bin/ignition.sh
+++ bin/ignition.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BRAINLORD_PATH="/Users/rfontanarosa/git"
+BRAINLORD_PATH="$HOME/git"
 RESOURCE_PATH="$BRAINLORD_PATH/brainlordresources/ignition"
 TOOLS_PATH="$BRAINLORD_PATH/brainlordtools/brainlordtools"
 
