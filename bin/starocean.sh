5bd95f11f5f0e600d82c368534dc53f5f75f4b6a
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2024-07-16 23:47:55 +0200

    starocean - improved misc dumper and misc inserter

--- bin/starocean.sh
+++ bin/starocean.sh
@@ -9,6 +9,7 @@ SOURCE_ES="$RESOURCE_PATH/roms/Star_Ocean_ES.sfc"
 DESTINATION="$RESOURCE_PATH/roms/Star_Ocean_IT.sfc"
 
 TABLE1="$RESOURCE_PATH/tables/star_ocean_ES_small.tbl"
+TABLE2="$RESOURCE_PATH/tables/star_ocean_ES_small.tbl"
 
 DUMP_TEXT_PATH="$RESOURCE_PATH/dump_text"
 DUMP_MISC_PATH="$RESOURCE_PATH/dump_misc"
@@ -18,15 +19,18 @@ TRANSLATION_TEXT_PATH="$RESOURCE_PATH/translation_text"
 TRANSLATION_MISC_PATH="$RESOURCE_PATH/translation_misc"
 TANSLATION_GFX_PATH="$RESOURCE_PATH/translation_gfx"
 
+python "$TOOLS_PATH/_utils.py" file_copy -s "$SOURCE_ES" -d "$DESTINATION"
+
 CURRENT_PATH=$PWD
 cd $BRAINLORD_PATH/Romhacking
-source ./venv/bin/activate
+# source ./venv/bin/activate
 # python -m starocean.main extract $RESOURCE_PATH/roms/Star_Ocean_U.sfc $DUMP_TEXT_PATH/dump_eng.txt
 # python -m starocean.main reinsert $SOURCE $TRANSLATION_TEXT_PATH/dump_ita.txt
-deactivate
+# deactivate
 cd $CURRENT_PATH
 
-python "$TOOLS_PATH/starocean.py" dump_gfx -s "$SOURCE" -dp "$DUMP_GFX_PATH"
 python "$TOOLS_PATH/starocean.py" dump_misc -s "$SOURCE_ES" -t1 "$TABLE1" -dp "$DUMP_MISC_PATH"
+# python "$TOOLS_PATH/starocean.py" dump_gfx -s "$SOURCE" -dp "$DUMP_GFX_PATH"
 
-python "$TOOLS_PATH/starocean.py" insert_gfx -d "$DESTINATION" -tp "$TANSLATION_GFX_PATH"
+python "$TOOLS_PATH/starocean.py" insert_misc -d "$DESTINATION" -t1 "$TABLE2" -t2 "$TABLE2" -tp "$TRANSLATION_MISC_PATH"
+# python "$TOOLS_PATH/starocean.py" insert_gfx -d "$DESTINATION" -tp "$TANSLATION_GFX_PATH"
--- brainlordtools/starocean.py
+++ brainlordtools/starocean.py
@@ -4,15 +4,79 @@ __version__ = ""
 __maintainer__ = "Roberto Fontanarosa"
 __email__ = "robertofontanarosa@gmail.com"
 
-import os, shutil, struct
-from rhutils.dump import dump_binary, get_csv_translated_texts, insert_binary
+import csv, os, shutil, struct, sys
+
+from rhutils.dump import dump_binary, insert_binary, get_csv_translated_texts, read_text
 from rhutils.table import Table
+
+def starocean_misc_dumper(args):
+    source_file = args.source_file
+    table1_file = args.table1
+    dump_path = args.dump_path
+    table = Table(table1_file)
+    shutil.rmtree(dump_path, ignore_errors=True)
+    os.mkdir(dump_path)
+    with open(source_file, 'rb') as f, open(source_file, 'rb') as f1:
+        # Menu
+        filename = os.path.join(dump_path, 'menu.csv')
+        with open(filename, 'w+', encoding='utf-8') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(['text_address', 'text', 'trans'])
+            pointers = {}
+            f.seek(0x3f7a4c)
+            while f.tell() < 0x3f7d3a:
+                pointers[f.tell()] = struct.unpack('H', f.read(2))[0] + 0x3f802a
+            for key, value in pointers.items():
+                text = read_text(f1, value, end_byte=b'\xff')
+                text_decoded = table.decode(text)
+                fields = [hex(value), text_decoded]
+                csv_writer.writerow(fields)
+        # Items
+        filename = os.path.join(dump_path, 'items.csv')
+        with open(filename, 'w+', encoding='utf-8') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(['text_address', 'text', 'trans'])
+            pointers = {}
+            f.seek(0x3c959d)
+            while f.tell() < 0x3c9c97:
+                pointers[f.tell()] = struct.unpack('H', f.read(2))[0] + 0x3ca393
+            for key, value in pointers.items():
+                text = read_text(f1, value, end_byte=b'\xff')
+                text_decoded = table.decode(text)
+                fields = [hex(value), text_decoded]
+                csv_writer.writerow(fields)
+
+def starocean_misc_inserter(args):
+    dest_file = args.dest_file
+    table1_file = args.table1
+    table2_file = args.table2
+    translation_path = args.translation_path
+    table = Table(table1_file)
+    table2 = Table(table2_file)
+    with open(dest_file, 'r+b') as f1, open(dest_file, 'r+b') as f2:
+        # Menu
+        f2.seek(0x3f7a4c)
+        f1.seek(0x3f802a)
+        translation_file = os.path.join(translation_path, 'menu.csv')
+        translated_texts = get_csv_translated_texts(translation_file)
+        already_pointed = {}
+        for i, (t_address, t_value) in enumerate(translated_texts):
+            if not already_pointed.get(t_address):
+                already_pointed[t_address] = f1.tell()
+                encoded_text = table2.encode(t_value) + b'\xff'
+                if f1.tell() + len(encoded_text) > 0x3f9433 and f1.tell() + len(encoded_text) < 0x3fa440:
+                    f1.seek(0x3fa440)
+                f1.write(encoded_text)
+            pointer = struct.pack('H', (already_pointed.get(t_address, f1.tell()) - 0x802a) & 0x00FFFF)
+            f2.write(pointer)
+        if f1.tell() > 0x3fa4a0:
+            print(f'Text exceeds: {0x3f9433 - f1.tell()}')
+        # Items
+            # limit 0x3cc48b
 
 def starocean_gfx_dumper(args):
     source_file = args.source_file
     dump_path = args.dump_path
-    # if not args.no_crc32_check and crc32(source_file) != CRC32:
-    #     sys.exit('SOURCE ROM CHECKSUM FAILED!')
     shutil.rmtree(dump_path, ignore_errors=True)
     os.mkdir(dump_path)
     with open(source_file, 'rb') as f:
@@ -26,29 +90,21 @@ def starocean_gfx_inserter(args):
         insert_binary(f, 0x3f0000, 0x3f0000 + 3392, translation_path, '3F0000_font_ita.bin')
         insert_binary(f, 0x3f0d40, 0x3f0d40 + (4 * 13) + 1, translation_path, '3F0d40_font_vwf.bin')
 
-def starocean_misc_inserter(args):
-    dest_file = args.dest_file
-    table1_file = args.table1
-    table2_file = args.table2
-    translation_path = args.translation_path
-    table = Table(table1_file)
-    table2 = Table(table2_file)
-    with open(dest_file, 'r+b') as f1, open(dest_file, 'r+b') as f2:
-        # Items
-        f2.seek(0x3f7a4c)
-        f1.seek(0x3f802a)
-        translation_file = os.path.join(translation_path, 'items.csv')
-        translated_texts = get_csv_translated_texts(translation_file)
-        for i, (t_address, t_value) in enumerate(translated_texts.items()):
-            pointer = struct.pack('H', (f1.tell() - 0x802a) & 0x00FFFF)
-            f2.write(pointer)
-            encoded_text = table2.encode(t_value)
-            f1.write(encoded_text + b'\xff')
-
 import argparse
 parser = argparse.ArgumentParser()
 parser.set_defaults(func=None)
 subparsers = parser.add_subparsers()
+dump_misc_parser = subparsers.add_parser('dump_misc', help='Execute MISC DUMPER')
+dump_misc_parser.add_argument('-s', '--source', action='store', dest='source_file', required=True, help='Original filename')
+dump_misc_parser.add_argument('-t1', '--table1', action='store', dest='table1', help='Original table filename')
+dump_misc_parser.add_argument('-dp', '--dump_path', action='store', dest='dump_path', help='Dump path')
+dump_misc_parser.set_defaults(func=starocean_misc_dumper)
+insert_misc_parser = subparsers.add_parser('insert_misc', help='Execute MISC INSERTER')
+insert_misc_parser.add_argument('-d', '--dest', action='store', dest='dest_file', required=True, help='Destination filename')
+insert_misc_parser.add_argument('-t1', '--table1', action='store', dest='table1', help='Original table filename')
+insert_misc_parser.add_argument('-t2', '--table2', action='store', dest='table2', help='Modified table filename')
+insert_misc_parser.add_argument('-tp', '--translation_path', action='store', dest='translation_path', help='Translation path')
+insert_misc_parser.set_defaults(func=starocean_misc_inserter)
 dump_gfx_parser = subparsers.add_parser('dump_gfx', help='Execute GFX DUMP')
 dump_gfx_parser.add_argument('-s', '--source', action='store', dest='source_file', required=True, help='Original filename')
 dump_gfx_parser.add_argument('-dp', '--dump_path', action='store', dest='dump_path', help='Dump path')
@@ -57,12 +113,6 @@ insert_gfx_parser = subparsers.add_parser('insert_gfx', help='Execute GFX INSERT
 insert_gfx_parser.add_argument('-d', '--dest', action='store', dest='dest_file', required=True, help='Destination filename')
 insert_gfx_parser.add_argument('-tp', '--translation_path', action='store', dest='translation_path', help='Translation path')
 insert_gfx_parser.set_defaults(func=starocean_gfx_inserter)
-insert_misc_parser = subparsers.add_parser('insert_misc', help='Execute MISC INSERTER')
-insert_misc_parser.add_argument('-d', '--dest', action='store', dest='dest_file', required=True, help='Destination filename')
-insert_misc_parser.add_argument('-t1', '--table1', action='store', dest='table1', help='Original table filename')
-insert_misc_parser.add_argument('-t2', '--table2', action='store', dest='table2', help='Modified table filename')
-insert_misc_parser.add_argument('-tp', '--translation_path', action='store', dest='translation_path', help='Translation path')
-insert_misc_parser.set_defaults(func=starocean_misc_inserter)
 
 if __name__ == "__main__":
     args = parser.parse_args()
