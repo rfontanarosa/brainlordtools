208c136fd8894a5e32a6daf2deb2e940edb6cbe2
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2024-08-07 16:44:23 +0200

    removed starocean chester tool reference

--- bin/starocean.sh
+++ bin/starocean.sh
@@ -21,16 +21,8 @@ TANSLATION_GFX_PATH="$RESOURCE_PATH/translation_gfx"
 
 python "$TOOLS_PATH/_utils.py" file_copy -s "$SOURCE_ES" -d "$DESTINATION"
 
-CURRENT_PATH=$PWD
-cd $BRAINLORD_PATH/Romhacking
-# source ./venv/bin/activate
-# python -m starocean.main extract $RESOURCE_PATH/roms/Star_Ocean_U.sfc $DUMP_TEXT_PATH/dump_eng.txt
-# python -m starocean.main reinsert $SOURCE $TRANSLATION_TEXT_PATH/dump_ita.txt
-# deactivate
-cd $CURRENT_PATH
-
 python "$TOOLS_PATH/starocean.py" dump_misc -s "$SOURCE_ES" -t1 "$TABLE1" -dp "$DUMP_MISC_PATH"
-# python "$TOOLS_PATH/starocean.py" dump_gfx -s "$SOURCE" -dp "$DUMP_GFX_PATH"
+python "$TOOLS_PATH/starocean.py" dump_gfx -s "$SOURCE_ES" -dp "$DUMP_GFX_PATH"
 
 python "$TOOLS_PATH/starocean.py" insert_misc -d "$DESTINATION" -t1 "$TABLE2" -t2 "$TABLE2" -tp "$TRANSLATION_MISC_PATH"
-# python "$TOOLS_PATH/starocean.py" insert_gfx -d "$DESTINATION" -tp "$TANSLATION_GFX_PATH"
+python "$TOOLS_PATH/starocean.py" insert_gfx -d "$DESTINATION" -tp "$TANSLATION_GFX_PATH"
