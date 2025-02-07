ba4539fce7e67711488889a8e2dbb77b3ec6c79d
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2025-02-07 23:53:32 +0100

    gaia - moved command list outside the block

--- brainlordtools/gaia.py
+++ brainlordtools/gaia.py
@@ -18,6 +18,8 @@ EXP_SIZE = 0x80000
 
 pointer_offsets_to_exclude = (0x5c94, 0x64e1c, 0xa0f3e, 0xaf75e, 0xdbdc, 0x11318, 0xd2ebc, 0x13321, 0x329b9, 0x80f93, 0xbe1ec, 0xba2dc, 0xba4f3, 0x80f93, 0xaedf9, 0xa468b, 0x226ab, 0x31732, 0xd3016, 0xdd242, 0xdd244, 0xdf92a, 0xe7728, 0xe7e76, 0xf3496, 0x1032df, 0x103c2a, 0x10d8c7, 0x118258, 0x12846b, 0x12b8f4, 0x13c9dd, 0x13f292, 0x14bd72, 0x151bc7, 0x1527eb, 0x15637d, 0x16c614, 0x16c61e, 0x171027, 0x184e15, 0x188d3b, 0x1909f0, 0x19853c, 0x1ddd09, 0x1e6886, 0x1ebcf8, 0x1edec7, 0x1f0973, 0x1f0b8b, 0x1f0bcb, 0x1f189b, 0x1f8cb2, 0x1f8e28, 0x1f9e90)
 
+cmd_list = {b'\xc1': 2, b'\xc3': 1, b'\xc5': 4, b'\xc6': 4, b'\xc7': 2, b'\xc9': 1, b'\xcd': 3, b'\xd1': 2, b'\xd2': 1, b'\xd5': 1, b'\xd6': 1, b'\xd7': 1,}
+
 def gaia_read_text(f, offset=None, length=None, end_byte=None, cmd_list=None, append_end_byte=False):
     text = b''
     if offset is not None:
@@ -76,7 +78,7 @@ def gaia_text_dumper(args):
             taddress = pointer[0] + (pointer[1] << 8) + bank_byte
             pointers.setdefault(taddress, []).append(pointer_offset)
         for i, (taddress, paddresses) in enumerate(pointers.items()):
-            text = gaia_read_text(f, taddress, end_byte=(b'\xc0', b'\xca'), cmd_list={b'\xc1': 2, b'\xc3': 1, b'\xc5': 4, b'\xc6': 4, b'\xc7': 2, b'\xc9': 1, b'\xcd': 3, b'\xd1': 2, b'\xd2': 1, b'\xd5': 1, b'\xd6': 1, b'\xd7': 1,}, append_end_byte=True)
+            text = gaia_read_text(f, taddress, end_byte=(b'\xc0', b'\xca'), cmd_list=cmd_list, append_end_byte=True)
             text_decoded = table.decode(text, mte_resolver=True, dict_resolver=True)
             pointer_addresses = ';'.join(hex(x) for x in paddresses)
             ref = f'[BLOCK {id}: {hex(taddress)} to {hex(f.tell() - 1)} - {pointer_addresses}]'
