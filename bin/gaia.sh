7ad313991c41d5dc9699253a28c95495bfc94bd2
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2025-05-01 19:24:49 +0200

    gaia - added missing parameters

--- bin/gaia.sh
+++ bin/gaia.sh
@@ -26,11 +26,11 @@ TANSLATION_GFX_PATH="$RESOURCE_PATH/translation_gfx"
 python "$TOOLS_PATH/_utils.py" file_copy -s "$SOURCE" -d "$DESTINATION"
 python "$TOOLS_PATH/gaia.py" expand -d "$DESTINATION"
 
-python "$TOOLS_PATH/gaia.py" dump_text -s "$SOURCE" -t1 "$TABLE1" -dp "$DUMP_TEXT_PATH" -db "$DB"
+python "$TOOLS_PATH/gaia.py" dump_text -s "$SOURCE" -t1 "$TABLE1" -t3 "$TABLE4" -dp "$DUMP_TEXT_PATH" -db "$DB"
 python "$TOOLS_PATH/gaia.py" dump_misc -s "$SOURCE" -t1 "$TABLE1" -t2 "$TABLE3" -t3 "$TABLE4" -dp "$DUMP_MISC_PATH"
 python "$TOOLS_PATH/gaia.py" dump_gfx -s "$SOURCE" -dp "$DUMP_GFX_PATH"
 
-python "$TOOLS_PATH/gaia.py" insert_text -s "$SOURCE" -d "$DESTINATION" -t2 "$TABLE2" -tp "$TRANSLATION_TEXT_PATH" -db "$DB" -u "$USER"
+python "$TOOLS_PATH/gaia.py" insert_text -s "$SOURCE" -d "$DESTINATION" -t2 "$TABLE2" -t3 "$TABLE5" -tp "$TRANSLATION_TEXT_PATH" -db "$DB" -u "$USER"
 python "$TOOLS_PATH/gaia.py" insert_misc -d "$DESTINATION" -t1 "$TABLE2" -t2 "$TABLE3" -t3 "$TABLE5" -tp "$TRANSLATION_MISC_PATH"
 python "$TOOLS_PATH/gaia.py" insert_gfx -d "$DESTINATION" -tp "$TANSLATION_GFX_PATH"
 
