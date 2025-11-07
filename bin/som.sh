3e91109f5dd32ee873914804ef57c7528ec9e514
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2025-11-07 17:24:53 +0100

    som - added reference to sadnes rom

--- bin/som.sh
+++ bin/som.sh
@@ -7,7 +7,7 @@ TOOLS_PATH="$BRAINLORD_PATH/brainlordtools/brainlordtools"
 USER="clomax"
 DB="$RESOURCE_PATH/db/som.sqlite3"
 SOURCE="$RESOURCE_PATH/roms/Secret of Mana (USA).sfc"
-# SOURCE="$RESOURCE_PATH/roms/Secret of Mana (ITA - SADNESS).sfc"
+SOURCE1="$RESOURCE_PATH/roms/Secret of Mana (ITA - SADNESS).sfc"
 DESTINATION="$RESOURCE_PATH/roms/Secret of Mana (ITA).sfc"
 
 TABLE1="$RESOURCE_PATH/tables/som_main.tbl"
@@ -29,6 +29,8 @@ python "$TOOLS_PATH/_utils.py" file_copy -s "$SOURCE" -d "$DESTINATION"
 
 python "$TOOLS_PATH/som.py" dump_text -s "$SOURCE" -t1 "$TABLE1" -dp "$DUMP_TEXT_PATH" -db "$DB"
 python "$TOOLS_PATH/som.py" dump_misc -s "$SOURCE" -t1 "$TABLE1" -dp "$DUMP_MISC_PATH"
+# python "$TOOLS_PATH/som.py" --no_crc32_check dump_text -s "$SOURCE1" -t1 "$TABLE3" -dp "$DUMP_TEXT_PATH" -db "$DB"
+# python "$TOOLS_PATH/som.py" --no_crc32_check dump_misc -s "$SOURCE1" -t1 "$TABLE1" -dp "$DUMP_MISC_PATH"
 
 python "$TOOLS_PATH/som.py" insert_text -s "$SOURCE" -d "$DESTINATION" -t2 "$TABLE4" -tp "$TRANSLATION_TEXT_PATH" -db "$DB" -u "$USER"
 python "$TOOLS_PATH/som.py" insert_misc -d "$DESTINATION" -t1 "$TABLE5" -tp "$TRANSLATION_MISC_PATH"
