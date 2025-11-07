44e2c9db39bf0e909f98e1ddb9db43c9a3558c57
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2025-11-07 17:19:49 +0100

    added order by to queries

--- brainlordtools/rhutils/db.py
+++ brainlordtools/rhutils/db.py
@@ -15,14 +15,23 @@ def insert_translation(cur, id_text, project, author, translation, status, date,
 def update_translation(cur, id_text, project, author, translation, status, date, tags, comment):
     cur.execute('UPDATE translations SET translation=?, status=?, date=?, tags=?, comment=? WHERE id_text=? AND project=? AND author=?', (translation, status, date, tags, comment, id_text, project, author))
 
+def select_texts(cur, blocks=None):
+    query = f"SELECT t1.id, t1.text_decoded FROM texts AS t1"
+    if blocks:
+        query += " WHERE t1.block IN ({})".format(', '.join(blocks))
+    query += " ORDER BY t1.id"
+    return cur.execute(query)
+
 def select_translation_by_author(cur, author, blocks=None):
     query = f"SELECT t1.id, t1.text, t1.text_decoded, t1.address, t1.pointer_addresses, t2.translation, t1.ref FROM texts AS t1 LEFT OUTER JOIN (SELECT * FROM translations WHERE author='{author}' AND status = 2) AS t2 ON t1.id=t2.id_text"
     if blocks:
         query += " WHERE t1.block IN ({})".format(', '.join(blocks))
+    query += " ORDER BY t1.id"
     return cur.execute(query)
 
 def select_most_recent_translation(cur, blocks=None):
     query = "SELECT * FROM (SELECT t1.id, t1.text, t1.text_decoded, t1.address, t1.pointer_addresses, t2.translation, t2.author, COALESCE(t2.date, 1) AS date, t1.ref FROM texts AS t1 LEFT OUTER JOIN (SELECT * FROM translations WHERE status = 2) AS t2 ON t1.id=t2.id_text) WHERE 1=1 GROUP BY id HAVING MAX(date)"
     if blocks:
         query = "SELECT * FROM (SELECT t1.id, t1.text, t1.text_decoded, t1.address, t1.pointer_addresses, t2.translation, t2.author, COALESCE(t2.date, 1) AS date, t1.ref FROM texts AS t1 LEFT OUTER JOIN (SELECT * FROM translations WHERE status = 2) AS t2 ON t1.id=t2.id_text WHERE t1.block IN ({})) WHERE 1=1 GROUP BY id HAVING MAX(date)".format(', '.join(blocks))
+    query += " ORDER BY t1.id"
     return cur.execute(query)
