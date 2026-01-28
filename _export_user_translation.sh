e735d95e46b5448a2cae8002d2ce7538f44f0522
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2026-01-28 17:24:57 +0100

    removed spike and ignition utils

--- _export_user_translation.sh
+++ _export_user_translation.sh
@@ -8,6 +8,17 @@ RESOURCE_PATH="$BRAINLORD_PATH/brainlordresources/$GAME_ID"
 DB="$RESOURCE_PATH/db/$GAME_ID.sqlite3"
 
 case $GAME_ID in
+  "ignition" | "spike")
+    DUMP_DIR="$RESOURCE_PATH/translation_text"
+    DEST_FILE="$DUMP_DIR/dump_ita_$USER.txt"
+
+    mkdir -p "$DUMP_DIR"
+    echo "Processing $GAME_ID for user $USER..."
+    python -m brainlordutils.export_user_translation export_user_translation \
+        -u "$USER" -db "$DB" -d "$DEST_FILE"
+    echo "Done! Files saved in $DUMP_DIR"
+    ;;
+
   "som")
     DUMP_DIR="$RESOURCE_PATH/translation_text"
     DEST_EVENTS_FILE="$DUMP_DIR/dump_events_$USER.txt"
--- _import_user_translation.sh
+++ _import_user_translation.sh
@@ -2,13 +2,23 @@
 
 GAME_ID=${1:-"som"}
 USER1=${2:-"clomax"}
-USER2=${3:-"mog"}
+USER2=${3:-"xxx"}
 
 BRAINLORD_PATH="$HOME/git"
 RESOURCE_PATH="$BRAINLORD_PATH/brainlordresources/$GAME_ID"
 DB="$RESOURCE_PATH/db/$GAME_ID.sqlite3"
 
 case $GAME_ID in
+  "ignition" | "spike")
+    DUMP_DIR="$RESOURCE_PATH/translation_text"
+    SOURCE_FILE="$DUMP_DIR/dump_ita_$USER1.txt"
+
+    echo "Processing $GAME_ID for user $USER1..."
+    python -m brainlordutils.import_user_translation import_user_translation \
+        -u "$USER2" -db "$DB" -s "$SOURCE_FILE"
+    echo "Done! Files have been imported into $DB"
+    ;;
+
   "som")
     DUMP_DIR="$RESOURCE_PATH/translation_text"
     SOURCE_EVENTS_FILE="$DUMP_DIR/dump_events_$USER1.txt"
