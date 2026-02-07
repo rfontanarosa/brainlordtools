254dc303b062601cc90e4b9dfedfab0cc00102fb
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2026-02-07 14:41:25 +0100

    improved error management

--- bin/7thsaga.sh
+++ bin/7thsaga.sh
@@ -21,7 +21,7 @@ TANSLATION_GFX_PATH="$RESOURCE_PATH/translation_gfx"
 TRANSLATION_MISC_PATH="$RESOURCE_PATH/translation_misc"
 
 python "$TOOLS_PATH/_utils.py" crc_check -s "$SOURCE" -g "$GAME_ID" || exit 1
-python "$TOOLS_PATH/_utils.py" copy_file -s "$SOURCE" -d "$DESTINATION"
+python "$TOOLS_PATH/_utils.py" copy_file -s "$SOURCE" -d "$DESTINATION" || exit 1
 
 python "$TOOLS_PATH/7thsaga.py" dump_text -s "$SOURCE" -t1 "$TABLE1" -dp "$DUMP_TEXT_PATH" -db "$DB"
 python "$TOOLS_PATH/7thsaga.py" dump_gfx -s "$SOURCE" -dp "$DUMP_GFX_PATH"
--- bin/ruinarm.sh
+++ bin/ruinarm.sh
@@ -16,7 +16,8 @@ DUMP_TEXT_PATH="$RESOURCE_PATH/dump_text"
 
 TRANSLATION_TEXT_PATH="$RESOURCE_PATH/translation_text"
 
-python "$TOOLS_PATH/_utils.py" copy_file -s "$SOURCE" -d "$DESTINATION"
+python "$TOOLS_PATH/_utils.py" crc_check -s "$SOURCE" -g "$GAME_ID" || exit 1
+python "$TOOLS_PATH/_utils.py" copy_file -s "$SOURCE" -d "$DESTINATION" || exit 1
 
 python "$TOOLS_PATH/ruinarm.py" dump_text -s "$SOURCE" -t1 "$TABLE1" -dp "$DUMP_TEXT_PATH" -db "$DB"
 
