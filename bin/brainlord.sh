05ad40aa489a07e9aaddc125bc219095e57e86b2
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2026-02-09 01:38:02 +0100

    brainlord - updated shell script

--- bin/brainlord.sh
+++ bin/brainlord.sh
@@ -9,9 +9,11 @@ DB="$RESOURCE_PATH/db/$GAME_ID.sqlite3"
 SOURCE="$RESOURCE_PATH/roms/Brain Lord (U) [!].sfc"
 DESTINATION="$RESOURCE_PATH/roms/Brain Lord (I) [!].sfc"
 
-TABLE1="$RESOURCE_PATH/tables/Brain Lord (U) [!].tbl"
-TABLE2="$RESOURCE_PATH/tables/Brain Lord (I) [!].tbl"
-TABLE3="$RESOURCE_PATH/tables/bl.tbl"
+TABLE1="$RESOURCE_PATH/tables/brain_lord_usa.tbl"
+TABLE2="$RESOURCE_PATH/tables/brain_lord_ita.tbl"
+TABLE3="$RESOURCE_PATH/tables/brain_lord_credits.tbl"
+TABLE4="$RESOURCE_PATH/tables/brain_lord_usa_without_dte.tbl"
+TABLE5="$RESOURCE_PATH/tables/brain_lord_ita_without_dte.tbl"
 
 DUMP_TEXT_PATH="$RESOURCE_PATH/dump_text"
 DUMP_MISC_PATH="$RESOURCE_PATH/dump_misc"
@@ -28,13 +30,13 @@ python "$TOOLS_PATH/_utils.py" copy_file -s "$SOURCE" -d "$DESTINATION" || exit 
 python "$TOOLS_PATH/_utils.py" expand -d "$DESTINATION" -g "$GAME_ID" || exit 1
 
 python "$TOOLS_PATH/brainlord.py" dump_text -s "$SOURCE" -t1 "$TABLE1" -dp "$DUMP_TEXT_PATH" -db "$DB"
-# python "$TOOLS_PATH/brainlord.py" dump_gfx -s "$SOURCE" -dp "$DUMP_GFX_PATH"
-# python "$TOOLS_PATH/brainlord.py" dump_misc -s "$SOURCE" -t1 "$TABLE1" -dp "$DUMP_MISC_PATH"
-# python "$TOOLS_PATH/brainlord.py" dump_credits -s "$SOURCE" -t3 "$TABLE3" -dp "$DUMP_CREDITS_PATH"
+python "$TOOLS_PATH/brainlord.py" dump_gfx -s "$SOURCE" -dp "$DUMP_GFX_PATH"
+python "$TOOLS_PATH/brainlord.py" dump_misc -s "$SOURCE" -t1 "$TABLE1" -t2 "$TABLE4" -dp "$DUMP_MISC_PATH"
+python "$TOOLS_PATH/brainlord.py" dump_credits -s "$SOURCE" -t3 "$TABLE3" -dp "$DUMP_CREDITS_PATH"
 
 python "$TOOLS_PATH/brainlord.py" insert_text -s "$SOURCE" -d "$DESTINATION" -t2 "$TABLE2" -tp "$TANSLATION_TEXT_PATH" -db "$DB" -u "$USER"
 python "$TOOLS_PATH/brainlord.py" insert_gfx -d "$DESTINATION" -tp "$TANSLATION_GFX_PATH"
-python "$TOOLS_PATH/brainlord.py" insert_misc -s "$SOURCE" -d "$DESTINATION" -t1 "$TABLE1" -t2 "$TABLE2" -tp "$TRANSLATION_MISC_PATH"
+python "$TOOLS_PATH/brainlord.py" insert_misc -s "$SOURCE" -d "$DESTINATION" -t1 "$TABLE2" -tp "$TRANSLATION_MISC_PATH"
 python "$TOOLS_PATH/brainlord.py" insert_credits -d "$DESTINATION" -t3 "$TABLE3" -tp "$TANSLATION_CREDITS_PATH"
 
 # SOURCE="$RESOURCE_PATH/roms/Brain Lord (I) [!] - 0x74000.sfc"
