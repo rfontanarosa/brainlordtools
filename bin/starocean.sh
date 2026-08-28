a299c5855513a9434c86733dba20db3362786723
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2026-08-28 11:21:54 +0200

    unlocked star ocean extraction command

--- bin/starocean.sh
+++ bin/starocean.sh
@@ -10,6 +10,6 @@ DESTINATION="$RESOURCE_PATH/roms/Star_Ocean_IT.sfc"
 
 CURRENT_PATH=$PWD
 cd $BRAINLORD_PATH/PrivateRomhacking
-# uv run starocean extract $SOURCE_EN $SOURCE_ES
+uv run starocean extract $SOURCE_EN $SOURCE_ES
 uv run starocean reinsert $SOURCE_ES
 cd $CURRENT_PATH
